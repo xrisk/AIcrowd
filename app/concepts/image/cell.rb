3265c6dffba597190e1ad1cc411269951ea7e422
Nate Jenkins <nate.jenkins@gmail.com>
2019-01-28 11:56:31 +0100

    force challenge cards to be 16x9 aspect ratio, fixes #787

--- app/concepts/image/cell.rb
+++ app/concepts/image/cell.rb
@@ -8,7 +8,7 @@ class Image::Cell < Template::Cell
     options[:klass]
   end
 
-  def image
+  def image_url
     if model.image_file
       image_url = model.image_file.url
       if image_url.nil?
@@ -17,7 +17,17 @@ class Image::Cell < Template::Cell
     else
       image_url = default_image_url
     end
+    image_url
+  end
+
+  def image
+    url = image_url
     image_tag(image_url, class: klass)
+  end
+
+  def image_16x9
+    url = image_url
+    image_tag(image_url, class: klass, style: "position: absolute;object-fit: cover;width: 100%;height: 100%;")
   end
 
   def default_image_url
