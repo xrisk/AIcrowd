72f4eeb41658eaee8bc78866a2e798fc4b21ffb3
Arjun Nemani <nemaniarjun@gmail.com>
2019-02-15 03:52:28 +0530

    Make sure validataion works for forms and shows feedback

--- app/assets/javascripts/modules/inline_validations.js
+++ app/assets/javascripts/modules/inline_validations.js
@@ -1,6 +1,8 @@
 // You will need to require 'jquery-ui' for this to work
 window.ClientSideValidations.callbacks.element.fail = function(element, message, callback) {
   callback();
+  element.removeClass("is-valid");
+  element.addClass("is-invalid");
   if (element.data('valid') !== false) {
     element.parent().find('.message').hide().show('slide', {direction: "left", easing: "easeOutBounce"}, 500);
   }
@@ -9,5 +11,7 @@ window.ClientSideValidations.callbacks.element.fail = function(element, message,
 window.ClientSideValidations.callbacks.element.pass = function(element, callback) {
   // Take note how we're passing the callback to the hide()
   // method so it is run after the animation is complete.
+  element.removeClass("is-invalid");
+  element.addClass("is-valid");
   element.parent().find('.message').hide('slide', {direction: "left"}, 500, callback);
 }
