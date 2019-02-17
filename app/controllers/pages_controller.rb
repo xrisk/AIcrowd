26283d551d605031a56dff24369a7cef28e5d448
Arjun Nemani <nemaniarjun@gmail.com>
2019-02-17 19:08:49 +0530

    Change the title depending on the page you are on

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,18 +1,23 @@
 class PagesController < ApplicationController
 
   def contact
+    @page_title = "Contact"
   end
 
   def privacy
+    @page_title = "Privacy"
   end
 
   def terms
+    @page_title = "Terms of Use"
   end
 
   def faq
+    @page_title = "FAQ"
   end
 
   def cookies
+    @page_title = "Cookies"
   end
 
 end
--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -1,6 +1,14 @@
 module MetaTagsHelper
   def meta_title
-    content_for?(:meta_title) ? content_for(:meta_title) : DEFAULT_META["meta_title"]
+    if content_for?(:meta_title)
+     content_for(:meta_title)
+    elsif !@page_title.blank?
+      "AIcrowd | " + @page_title
+    elsif title_from_controller_name != nil
+      "AIcrowd | " + title_from_controller_name
+    else
+      DEFAULT_META["meta_title"]
+    end
   end
 
   def meta_description
@@ -11,5 +19,28 @@ module MetaTagsHelper
     meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META["meta_image"])
     # little twist to make it work equally with an asset or a url
     meta_image.starts_with?("http") ? meta_image : url_to_image(meta_image)
+  end
+
+  def title_from_controller_name
+    case controller.controller_name
+    when 'challenges'
+      'Challenges'
+    when 'leaderboards'
+      'Leaderboard'
+    when 'dataset_files'
+      'Dataset'
+    when 'task_dataset_files'
+      'Task Dataset'
+    when 'participant_challenges'
+      'Participants'
+    when 'winners'
+      'Winners'
+    when 'submissions'
+      'Submissions'
+    when 'clef_tasks'
+      'Tasks'
+    when 'blogs'
+      'Blog'
+    end
   end
 end
