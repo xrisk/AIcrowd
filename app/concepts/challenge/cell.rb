e7dc7f31a9887134cf34e1b09bb8bef29527be4f
Arjun Nemani <nemaniarjun@gmail.com>
2019-07-21 19:31:20 +0530

    Challenge Masthead: Fix the time-remaining text
    This commit fixes the time remaining text for the challenge
    masthead and unifies the text shown on challenge list page
    and challenge cards.
    Also removed excess code that is not being used.
    
    Fixes https://github.com/AIcrowd/AIcrowd-Design/issues/100

--- app/concepts/challenge/cell.rb
+++ app/concepts/challenge/cell.rb
@@ -8,43 +8,64 @@ class Challenge::Cell < Template::Cell
     @current_challenge_round ||= challenge.challenge_round_summaries.where(round_status_cd: 'current').first
   end
 
-  def ending_dttm
-    return nil if current_challenge_round.nil?
-    current_challenge_round.end_dttm.strftime("%d %b %H:%M UTC")
+  def ending_dttm(challenge_round)
+    return nil if challenge_round.nil?
+    challenge_round.end_dttm.strftime("%d %b %H:%M UTC")
   end
 
-  def ending_time
-    return nil if current_challenge_round.nil?
-    current_challenge_round.end_dttm.strftime("%H:%M UTC")
+  def ending_time(challenge_round)
+    return nil if challenge_round.nil?
+    challenge_round.end_dttm.strftime("%H:%M UTC")
   end
 
-  def duration_in_seconds
-    return nil if current_challenge_round.nil?
-    current_challenge_round.end_dttm - current_challenge_round.start_dttm
+  def duration_in_seconds(challenge_round)
+    return nil if challenge_round.nil?
+    challenge_round.end_dttm - challenge_round.start_dttm
   end
 
-  def remaining_time_in_seconds
-    return 0 if current_challenge_round.nil? || current_challenge_round.end_dttm.nil?
-    seconds = current_challenge_round.end_dttm - Time.now
+  def remaining_time_in_seconds(challenge_round)
+    return 0 if challenge_round.nil? || challenge_round.end_dttm.nil?
+    seconds = challenge_round.end_dttm - Time.now
     if seconds.nil? || seconds < 0
       seconds = 0
     end
     return seconds
   end
 
-  def remaining_time_in_hours
-    (remaining_time_in_seconds / (60 * 60)).floor
+  def remaining_time_in_hours(challenge_round)
+    (remaining_time_in_seconds(challenge_round) / (60 * 60)).floor
   end
 
-  def remaining_time_in_days
-    (remaining_time_in_seconds / (60 * 60 * 24)).floor
+  def remaining_time_in_days(challenge_round)
+    (remaining_time_in_seconds(challenge_round) / (60 * 60 * 24)).floor
   end
 
-  def pct_remaining
-    if remaining_time_in_seconds > 0 && duration_in_seconds > 0
-      ((remaining_time_in_seconds / duration_in_seconds) * 100).floor
+  def pct_remaining(challenge_round)
+    if remaining_time_in_seconds(challenge_round) > 0 && duration_in_seconds > 0
+      ((remaining_time_in_seconds(challenge_round) / duration_in_seconds) * 100).floor
     else
       0
+    end
+  end
+
+  def remaining_text(challenge_round = current_challenge_round)
+    case challenge.status
+    when :running, :perpetual
+      if remaining_time_in_days(challenge_round) >= 2
+        "#{pluralize(remaining_time_in_days(challenge_round), 'day')} left"
+      elsif remaining_time_in_hours(challenge_round) > 0
+        "#{pluralize(remaining_time_in_hours(challenge_round), 'hour')} left &middot; Ending #{ending_dttm(challenge_round)}"
+      elsif remaining_time_in_seconds(challenge_round) > 0
+        "Less than 1 hour left &middot; Ending #{ending_dttm(challenge_round)}"
+      else
+        "Completed"   # display for perpetual challenges
+      end
+    when :draft
+      "Draft"
+    when :completed
+      "Completed"
+    when :starting_soon
+      "Starting soon"
     end
   end
 
--- app/concepts/challenge/cells/list_detail.rb
+++ app/concepts/challenge/cells/list_detail.rb
@@ -8,27 +8,4 @@ class Challenge::Cell::ListDetail < Challenge::Cell
     model
   end
 
-  def remaining_text
-    case challenge.status
-    when :running, :perpetual
-      if remaining_time_in_days > 7
-        "#{pluralize(remaining_time_in_days,'day')} left"
-      elsif remaining_time_in_days <= 7 and remaining_time_in_days >= 1
-        "#{pluralize(remaining_time_in_days,'day')} left &middot; Ending #{ending_dttm}"
-      elsif remaining_time_in_hours > 0
-        "#{pluralize(remaining_time_in_hours,'hour')} left &middot; Ending #{ending_dttm}"
-      elsif remaining_time_in_seconds > 0
-        "Less than 1 hour left &middot; Ending #{ending_dttm}"
-      else
-        "Completed"   # display for perpetual challenges
-      end
-    when :draft
-      "Draft"
-    when :completed
-      "Completed"
-    when :starting_soon
-      "Starting soon"
-    end
-  end
-
 end
