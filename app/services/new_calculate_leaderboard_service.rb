531b911747728e2ee6e78479d103ff46e85003ac
Arjun Nemani <nemaniarjun@gmail.com>
2019-08-07 06:06:17 +0530

    Disentanglement Leaderboard: Final update to the leaderbaord calculation job

--- app/services/new_calculate_leaderboard_service.rb
+++ app/services/new_calculate_leaderboard_service.rb
@@ -18,60 +18,33 @@ class NewCalculateLeaderboardService
   end
 
   def make_leaderboard
-    participant_set = Set.new
 
+    # Create Leaderboards for each Submission
     @submissions.each do |subm|
-      participant_set << subm.participant_id
-      extra_scores = subm.other_scores_array
-
-      DisentanglementLeaderboard.create!(
-          challenge_id: subm.challenge_id,
-          challenge_round_id: subm.challenge_round_id,
-          participant_id: subm.participant_id,
-          name: subm.name,
-          score: subm.score,
-          score_secondary: subm.score_secondary,
-          media_large: subm.media_large,
-          media_thumbnail: subm.media_thumbnail,
-          media_content_type: subm.media_content_type,
-          description: subm.description,
-          description_markdown: subm.description_markdown,
-          submission_id: subm.id,
-          post_challenge: subm.post_challenge,
-          meta: subm.meta,
-          previous_row_num: 0,
-          row_num: 0,
-          entries: @round.submissions.where(participant_id: subm.participant_id).count,
-          extra_score1: extra_scores[0],
-          extra_score2: extra_scores[1],
-          extra_score3: extra_scores[2],
-          extra_score4: extra_scores[3],
-          extra_score5: extra_scores[4]
-      )
+      create_leaderboard_from_submission(subm)
     end
 
-    participant_to_best_id_map = Hash.new
+    participant_to_best_submission_map = Hash.new
 
-    participant_set.each do |pid|
-      min_avg = 10000000000
-      best_id = 0
-
-      DisentanglementLeaderboard.where(challenge_round_id: @round.id, participant_id: pid).each do |dl|
-        dl.avg_rank = calculate_avg_rank(dl)
-        dl.save
-        if dl.avg_rank < min_avg
-          min_avg = dl.avg_rank
-          best_id = dl.id
-        end
-      end
-      participant_to_best_id_map[pid] = best_id
+    while DisentanglementLeaderboard.where(challenge_round_id: @round.id).count != 0
+      # Find participant with best rank
+      calculate_avg_ranks_for_all
+      curr_best = DisentanglementLeaderboard.where(challenge_round_id: @round.id).order(:avg_rank).first
+      curr_best_participant_id = curr_best.participant_id
+      curr_best_submission_id = curr_best.submission_id
+      # Save his submission id
+      participant_to_best_submission_map[curr_best_participant_id] = curr_best_submission_id
+      # Delete all the Leaderboards from this participant
+      DisentanglementLeaderboard.where(challenge_round_id: @round.id, participant_id: curr_best_participant_id).destroy_all
     end
 
-    participant_set.each do |pid|
-      DisentanglementLeaderboard
-          .where(challenge_round_id: @round.id, participant_id: pid)
-          .where.not(id: participant_to_best_id_map[pid]).destroy_all
+
+    participant_to_best_submission_map.each do |pid, sid|
+      subm = Submission.find(sid)
+      create_leaderboard_from_submission(subm)
     end
+
+    calculate_avg_ranks_for_all
 
     DisentanglementLeaderboard.where(challenge_round_id: @round.id).order(:avg_rank).each_with_index do |x, i|
       x.row_num = i + 1
@@ -80,14 +53,21 @@ class NewCalculateLeaderboardService
     end
   end
 
+  def calculate_avg_ranks_for_all
+    DisentanglementLeaderboard.where(challenge_round_id: @round.id).each do |dl|
+      dl.avg_rank = calculate_avg_rank(dl)
+      dl.save
+    end
+  end
+
   def calculate_avg_rank(entry)
-    @scores_to_avg = [ @challenge.score_title, @challenge.score_secondary_title ] + @challenge.other_scores_fieldnames_array
+    @scores_to_avg = [@challenge.score_title, @challenge.score_secondary_title] + @challenge.other_scores_fieldnames_array
     column_names = ['score', 'score_secondary'] + (1..@challenge.other_scores_fieldnames_array.length).map { |i| 'extra_score' + (i).to_s }
     leaderboard = DisentanglementLeaderboard.where(challenge_round_id: @round.id)
     submission = Submission.find(entry.submission_id)
     sum = 0.0
 
-    submission.meta.each do |k,v|
+    submission.meta.each do |k, v|
       if k.ends_with?('_rank')
         submission.meta.delete(k)
       end
@@ -95,8 +75,8 @@ class NewCalculateLeaderboardService
 
     @scores_to_avg.each_with_index do |name, i|
       col_name = column_names[i]
-      submission.meta[name+"_rank"] = leaderboard.where("#{col_name} > ?", entry.send(col_name)).count + 1
-      sum += submission.meta[name+"_rank"]
+      submission.meta[name + "_rank"] = leaderboard.where("#{col_name} > ?", entry.send(col_name)).count + 1
+      sum += submission.meta[name + "_rank"]
     end
 
     submission.meta['mean_rank'] = sum / @scores_to_avg.length
@@ -105,4 +85,32 @@ class NewCalculateLeaderboardService
     submission.meta['mean_rank']
   end
 
+  def create_leaderboard_from_submission(subm)
+    extra_scores = subm.other_scores_array
+
+    DisentanglementLeaderboard.create!(
+        challenge_id: subm.challenge_id,
+        challenge_round_id: subm.challenge_round_id,
+        participant_id: subm.participant_id,
+        name: subm.name,
+        score: subm.score,
+        score_secondary: subm.score_secondary,
+        media_large: subm.media_large,
+        media_thumbnail: subm.media_thumbnail,
+        media_content_type: subm.media_content_type,
+        description: subm.description,
+        description_markdown: subm.description_markdown,
+        submission_id: subm.id,
+        post_challenge: subm.post_challenge,
+        meta: subm.meta,
+        previous_row_num: 0,
+        row_num: 0,
+        entries: @round.submissions.where(participant_id: subm.participant_id).count,
+        extra_score1: extra_scores[0],
+        extra_score2: extra_scores[1],
+        extra_score3: extra_scores[2],
+        extra_score4: extra_scores[3],
+        extra_score5: extra_scores[4]
+    )
+  end
 end
