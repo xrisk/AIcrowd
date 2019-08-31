8281cd19d85061e14eb221176130934519841cb2
Arjun Nemani <nemaniarjun@gmail.com>
2019-08-31 05:47:53 +0530

    Fix test for the bug

--- spec/services/calculate_leaderboard_service_with_teams_spec.rb
+++ spec/services/calculate_leaderboard_service_with_teams_spec.rb
@@ -2,63 +2,65 @@ require 'rails_helper'
 
 RSpec.describe CalculateLeaderboardService do
   # challenge
-  let(:challenge) { create :challenge, :running, primary_sort_order: :descending }
-  let(:challenge_round) { challenge.challenge_rounds.first }
+  let(:challenge1) { create :challenge, :running, primary_sort_order: :descending }
+  let(:challenge2) { create :challenge, :running, primary_sort_order: :descending }
+  let(:challenge_round) { challenge1.challenge_rounds.first }
   # participants
-  let!(:p1_in_t1) { create :participant }
-  let!(:p2_in_t1) { create :participant }
-  let!(:p3_in_t2) { create :participant }
-  let!(:p4_solo) { create :participant }
+  let!(:p1) { create :participant }
+  let!(:p2) { create :participant }
+  let!(:p3) { create :participant }
+  let!(:p4) { create :participant }
   # teams
-  let!(:team1) { create :team, challenge: challenge, participants: [p1_in_t1, p2_in_t1] }
-  let!(:team2) { create :team, challenge: challenge, participants: [p3_in_t2] }
+  let!(:team1) { create :team, challenge: challenge1, participants: [p1, p2] }
+  let!(:team2) { create :team, challenge: challenge2, participants: [p1, p2] }
+  let!(:team3) { create :team, challenge: challenge1, participants: [p3] }
   # submissions
   let!(:p1s1) { create(:submission,
-    participant: p1_in_t1,
-    challenge: challenge,
-    challenge_round_id: challenge_round.id,
-    grading_status: :graded,
-    score: 30,
-    created_at: 50.hours.ago,
+                       participant: p1,
+                       challenge: challenge1,
+                       challenge_round_id: challenge_round.id,
+                       grading_status: :graded,
+                       score: 30,
+                       created_at: 50.hours.ago,
   ) }
   let!(:p2s1) { create(:submission,
-    participant: p2_in_t1,
-    challenge: challenge,
-    challenge_round_id: challenge_round.id,
-    grading_status: :graded,
-    score: 40,
-    created_at: 50.hours.ago,
+                       participant: p2,
+                       challenge: challenge1,
+                       challenge_round_id: challenge_round.id,
+                       grading_status: :graded,
+                       score: 40,
+                       created_at: 50.hours.ago,
   ) }
   let!(:p3s1) { create(:submission,
-    participant: p3_in_t2,
-    challenge: challenge,
-    challenge_round_id: challenge_round.id,
-    grading_status: :graded,
-    score: 20,
-    created_at: 50.hours.ago,
+                       participant: p3,
+                       challenge: challenge1,
+                       challenge_round_id: challenge_round.id,
+                       grading_status: :graded,
+                       score: 20,
+                       created_at: 50.hours.ago,
   ) }
   let!(:p4s1) { create(:submission,
-    participant: p4_solo,
-    challenge: challenge,
-    challenge_round_id: challenge_round.id,
-    grading_status: :graded,
-    score: 10,
-    created_at: 50.hours.ago,
+                       participant: p4,
+                       challenge: challenge1,
+                       challenge_round_id: challenge_round.id,
+                       grading_status: :graded,
+                       score: 10,
+                       created_at: 50.hours.ago,
   ) }
   let!(:p4s2) { create(:submission,
-    participant: p4_solo,
-    challenge: challenge,
-    challenge_round_id: challenge_round.id,
-    grading_status: :graded,
-    score: 5,
-    created_at: 10.hours.ago,
+                       participant: p4,
+                       challenge: challenge1,
+                       challenge_round_id: challenge_round.id,
+                       grading_status: :graded,
+                       score: 5,
+                       created_at: 10.hours.ago,
   ) }
 
   # Leaderboard
   # Row | Submission | Submitter    | Score | Entries
   # ----|------------|--------------|-------|--------
   #  1  |  p2s1      |  team1       |  40   |  2
-  #  2  |  p3s1      |  team2       |  20   |  1
+  #  2  |  p3s1      |  team3       |  20   |  1
   #  3  |  p4s1      |  p4_solo     |  10   |  2
 
 
@@ -75,11 +77,11 @@ RSpec.describe CalculateLeaderboardService do
     it { expect(Leaderboard.first.submitter).to eq(team1) }
 
     it { expect(Leaderboard.second.participant).not_to be }
-    it { expect(Leaderboard.second.submitter).to eq(team2) }
+    it { expect(Leaderboard.second.submitter).to eq(team3) }
     it { expect(Leaderboard.second.score).to eq(20) }
 
     it { expect(Leaderboard.third.score).to eq(10) }
-    it { expect(Leaderboard.third.submitter).to eq(p4_solo) }
+    it { expect(Leaderboard.third.submitter).to eq(p4) }
     it { expect(Leaderboard.third.entries).to eq(2) }
   end
 end
