2cb1900c06cda350bcec894358d6a943305d03c3
Arjun Nemani <nemaniarjun@gmail.com>
2019-09-26 00:21:00 +0530

    Active Admin: Add batch actions to re-evaluate submissions
    Batch Actions added to the Leaderboards and Submissions View.
    This will only work with submissions made with the web app, i.e.
    the ones that use broker to evaluate (simple CSV/JSON)

--- app/admin/submission.rb
+++ app/admin/submission.rb
@@ -2,13 +2,6 @@ ActiveAdmin.register Submission do
   belongs_to :challenge, parent_class: Challenge
   navigation_menu :challenge
 
-  sidebar "Submission Details", only: [:show, :edit] do
-    ul do
-      li link_to "Submission Files", admin_submission_submission_files_path(submission)
-      li link_to "Submission Grades", admin_submission_submission_grades_path(submission)
-    end
-  end
-
   filter :id
   filter :participant_id
   filter :name
@@ -19,21 +12,14 @@ ActiveAdmin.register Submission do
   filter :grading_status_cd
   filter :grading_message
 
-
   index do
     selectable_column
     column :id
     column :participant_id
-    column "Name" do |submission|
-      submission.name
-    end
-    column "Email" do |submission|
-      submission.email
-    end
+    column 'Name', &:name
+    column 'Email', &:email
     column :challenge_round_id
-    column "Round" do |submission|
-      submission.challenge_round.challenge_round
-    end
+    column 'Round' do |submission| submission.challenge_round.challenge_round end
     column :score
     column :score_secondary
     column :grading_status_cd
@@ -49,31 +35,33 @@ ActiveAdmin.register Submission do
     def scoped_collection
       super.includes :participant, :challenge_round
     end
+
     def find_resource
       scoped_collection.find(params[:id])
     end
+
     def permitted_params
       params.permit!
     end
   end
 
   form do |f|
-    f.inputs "Submission" do
+    f.inputs 'Submission' do
       f.input :challenge,
-        as: :select,
-        collection: Challenge.all.collect {
-          |challenge| [challenge.challenge, challenge.id] }
+              as: :select,
+              collection: Challenge.all.collect { |challenge|
+                            [challenge.challenge, challenge.id] }
       f.input :challenge_round_id
       f.input :participant_id,
-        label: 'Participant',
-        as: :select,
-        collection: Participant.all.order(:name).map{|u| ["#{u.name} - #{u.id}", u.id]}
+              label: 'Participant',
+              as: :select,
+              collection: Participant.all.order(:name).map { |u| ["#{u.name} - #{u.id}", u.id] }
       f.input :score
       f.input :score_secondary
       f.input :description_markdown
       f.input :grading_status,
-        as: :select,
-        collection: enum_option_pairs(Submission, :grading_status)
+              as: :select,
+              collection: enum_option_pairs(Submission, :grading_status)
       f.input :grading_message
       f.input :post_challenge
       f.input :media_large
@@ -84,7 +72,7 @@ ActiveAdmin.register Submission do
     f.actions
   end
 
- csv do
+  csv do
     column :id
     column :participant_id
     column(:participant) { |submission| submission.participant.name }
@@ -99,4 +87,10 @@ ActiveAdmin.register Submission do
     column :updated_at
   end
 
+  batch_action 'Re-Evaluate Submissions', priority: 1 do |ids|
+    ids.each do |sub_id|
+      SubmissionGraderJob.perform_later(sub_id)
+    end
+    redirect_to admin_challenge_submissions_path, alert: 'Submissions are being revaluated'
+  end
 end
