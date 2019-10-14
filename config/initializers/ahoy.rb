41b32bf880ac603cc49786e3b63299d700faac98
Arjun Nemani <nemaniarjun@gmail.com>
2019-10-14 13:05:07 +0530

    Fix Ahoy initializers

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -1,8 +1,22 @@
 class Ahoy::Store < Ahoy::DatabaseStore
   def user
-    controller.current_participant
+    controller.current_user
+  end
+
+  def authenticate(data)
+      # disables automatic linking of visits and users
   end
 end
 
 # set to true for JavaScript tracking
 Ahoy.api = false
+
+# Group ips for GDPR
+Ahoy.mask_ips = true
+
+# No Cookies #GDPR!
+Ahoy.cookies = false
+
+# Geocoding overloads our redis
+# Need db sideloaded without api calls
+Ahoy.geocode = false
