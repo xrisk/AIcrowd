d53dc5271ca99f0b8556fa4b874f9a378b6eb50a
Arjun Nemani <nemaniarjun@gmail.com>
2019-12-12 03:42:37 +0530

    Migration: Update Service, Add undo and check

--- app/services/migrate_user_service.rb
+++ app/services/migrate_user_service.rb
@@ -1,24 +1,51 @@
 class MigrateUserService
 
   def initialize(crowdai_id:, aicrowd_id:)
-    migrate_user(crowdai_id, aicrowd_id)
+    @old_id = crowdai_id
+    @new_id = aicrowd_id
   end
 
-  def migrate_user(old_id, new_id)
-    submission_migration_mappings = MigrationMapping.where(crowdai_participant_id: old_id, source_type: 'Submission')
+  def check_migrated
+    MigrationMapping.where(crowdai_participant_id: @old_id, source_type: 'Participant').count.positive? ? true : false
+  end
+
+  def migrate_user
+    raise "User Already Migrated" if MigrationMapping.where(crowdai_participant_id: @old_id, source_type: 'Participant').count.positive?
+
+    submission_migration_mappings = MigrationMapping.where(crowdai_participant_id: @old_id, source_type: 'Submission')
 
     submission_migration_mappings.each do |migration_mapping|
       submission = Submission.where(id: migration_mapping['source_id'])
-      submission.update(participant_id: new_id)
+      submission.update(participant_id: @new_id)
     end
 
-    cp_migration_mappings = MigrationMapping.where(crowdai_participant_id: old_id, source_type: 'ChallengeParticipant')
+    cp_migration_mappings = MigrationMapping.where(crowdai_participant_id: @old_id, source_type: 'ChallengeParticipant')
     cp_migration_mappings.each do |migration_mapping|
       cp = ChallengeParticipant.where(id: migration_mapping['source_id'])
-      cp.update(participant_id: new_id)
+      cp.update(participant_id: @new_id)
     end
 
-    MigrationMapping.create!(crowdai_participant_id: old_id, source_id: new_id, source_type: 'Participant')
+    MigrationMapping.create!(crowdai_participant_id: @old_id, source_id: @new_id, source_type: 'Participant')
   end
 
+
+  def undo_migration
+    user_migration = MigrationMapping.where(crowdai_participant_id: @old_id, source_id: @new_id, source_type: 'Participant').first
+    raise "User Not Migrated" if user_migration.nil?
+
+    submission_migration_mappings = MigrationMapping.where(crowdai_participant_id: @old_id, source_type: 'Submission')
+
+    submission_migration_mappings.each do |migration_mapping|
+      submission = Submission.where(id: migration_mapping['source_id'])
+      submission.update(participant_id: nil)
+    end
+
+    cp_migration_mappings = MigrationMapping.where(crowdai_participant_id: @old_id, source_type: 'ChallengeParticipant')
+    cp_migration_mappings.each do |migration_mapping|
+      cp = ChallengeParticipant.where(id: migration_mapping['source_id'])
+      cp.update(participant_id: nil)
+    end
+
+    user_migration.destroy
+  end
 end
