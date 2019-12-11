d3c15dcb2996233daad668bb57921f8f962af0aa
Arjun Nemani <nemaniarjun@gmail.com>
2019-12-12 02:25:53 +0530

    ActiveAdmin: Add Analytics Dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,16 +1,16 @@
 ActiveAdmin.register_page "Dashboard" do
 
-  menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
+  menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
-  content title: proc{ I18n.t("active_admin.dashboard") } do
+  content title: proc { I18n.t("active_admin.dashboard") } do
 
-    columns do
-      column do
-        panel "Background Jobs" do
-          render 'jobs'
-        end
+    tabs do
+      tab :Background_Jobs do
+        render 'jobs'
+      end
+      tab :Analytics_Dashboard do
+        render 'blazer'
       end
     end
-
   end # content
 end
