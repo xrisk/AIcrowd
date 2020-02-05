3a0afda8cfa4212e99b83963ee1f67805c8e5344
piotrekpasciak <piotrek.pasciak@gmail.com>
2020-02-05 10:42:09 +0100

    Refactor Submissions cells to partials

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -16,4 +16,59 @@ module SubmissionsHelper
       end
     end
   end
+
+  def submission_grade_class(submission)
+    if submission.grading_status_cd == 'graded'
+      return 'badge-success'
+    elsif submission.grading_status_cd == 'initiated'
+      return 'badge-gold'
+    elsif submission.grading_status_cd == 'submitted'
+      return 'badge-gold'
+    elsif submission.grading_status_cd == 'ready'
+      return 'badge-silver'
+    else
+      return 'badge-warning'
+    end
+  end
+
+  def submission_formatted_value(submission, value)
+    format("%.#{submission.challenge_round.score_precision}f", value || 0)
+  end
+
+  def submission_enable_links_in_raw_text(text)
+    return '-' if text.nil?
+
+    value = sanitize(text.gsub(%r{(https?://[\S]+)}, "<a href='\\1'>\\1</a>"))
+
+    return '-' if value.nil?
+
+    return value
+  end
+
+  def submission_meta_hash(submission)
+    meta = submission&.meta.presence
+    # Return hash if already parsed
+    return meta if meta.is_a? Hash
+    # Try to parse if not an hash
+    return JSON.parse(meta) if meta.is_a? String
+
+    return []
+  rescue JSON::ParserError
+    []
+  end
+
+  def submission_view_description(submission)
+    return false if submission.description.blank?
+    return true if current_participant&.admin? || current_participant&.id == submission.participant.id
+    return true if current_participant&.organizer&.id == submission.challenge.organizer.id
+  end
+
+
+  def submission_tab_classes(challenge_round, current_round_id)
+    if challenge_round.id == current_round_id
+      return 'nav-link active'
+    else
+      return 'nav-link'
+    end
+  end
 end
