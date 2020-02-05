b12c64bff58581883efc4758c9ac77badc1b4469
piotrekpasciak <piotrek.pasciak@gmail.com>
2020-02-05 13:35:11 +0100

    Refactor Leaderboard cells to partials

--- app/concepts/leaderboard/cells/table_head.rb
+++
@@ -1,5 +0,0 @@
-class Leaderboard::Cell::TableHead < Leaderboard::Cell
-  def show
-    render :table_head
-  end
-end
--- app/helpers/leaderboard_helper.rb
+++ app/helpers/leaderboard_helper.rb
@@ -21,35 +21,62 @@ module LeaderboardHelper
     ).html_safe
   end
 
-  def ranking_change_helper(entry)
-    if entry.previous_row_num == 0 || entry.previous_row_num == entry.row_num
-      output = "<td class='change'>" +
-        image_tag(
-          "icon-change-none.csv",
-          data:  { toggle: 'tooltip' },
-          title: '0 change') +
-        "</td>"
-      return output
+  def leaderboard_ranking_change(leaderboard)
+    if leaderboard.previous_row_num == 0 || leaderboard.previous_row_num == leaderboard.row_num
+      return image_tag(
+        "icon-change-none.svg",
+        data: { toggle: 'tooltip' },
+        title: 'No change')
     end
 
-    if entry.row_num > entry.previous_row_num
-      output = "<td class='change'>" +
-        image_tag(
-          "icon-change-up.csv",
-          data:  { toggle: 'tooltip' },
-          title: "+#{entry.row_num - entry.previous_row_num} change") +
-        "</td>"
-      return output
+    if leaderboard.row_num > leaderboard.previous_row_num
+      return image_tag("icon-change-down.svg",
+        data:  { toggle: 'tooltip' },
+        title: "-#{leaderboard.row_num - leaderboard.previous_row_num} change, previous rank #{leaderboard.previous_row_num}")
     end
 
-    if entry.row_num < entry.previous_row_num && entry.previous_row_num != 0
-      output = "<td class='change'>" +
-        image_tag(
-          "icon-change-down.csv",
-          data:  { toggle: 'tooltip' },
-          title: "-#{entry.previous_row_num - entry.row_num} change") +
-        "</td>"
-      return output
+    if leaderboard.row_num < leaderboard.previous_row_num && leaderboard.previous_row_num != 0
+      return image_tag(
+        "icon-change-up.svg",
+        data:  { toggle: 'tooltip' },
+        title: "+#{leaderboard.previous_row_num - leaderboard.row_num} change, previous rank #{leaderboard.previous_row_num}")
+    end
+  end
+
+  def leaderboard_participants(leaderboard)
+    temp_participants = begin
+      if leaderboard.try(:submitter_type) == 'Team'
+        leaderboard&.team&.participants&.to_a
+      else
+        [leaderboard&.participant]
+      end
+    end
+
+    temp_participants = [] if temp_participants.nil? || temp_participants.include?(nil)
+    return temp_participants
+  end
+
+  def leaderboard_formatted_value(challenge_round, value)
+    format("%.#{challenge_round.score_precision}f", value || 0)
+  end
+
+  def leaderboard_other_scores_array(leaderboard, challenge)
+    other_scores = []
+    challenge.other_scores_fieldnames_array.each do |fname|
+      other_scores << if leaderboard.meta && (leaderboard.meta.key? fname)
+                        (leaderboard.meta[fname].nil? ? "-" : format("%.3f", leaderboard.meta[fname].to_f))
+                      else
+                        '-'
+                      end
+    end
+    return other_scores
+  end
+
+  def leaderboard_tab_classes(challenge_round, current_round)
+    if challenge_round.id == current_round.id
+      return 'nav-link active'
+    else
+      return 'nav-link'
     end
   end
 end
