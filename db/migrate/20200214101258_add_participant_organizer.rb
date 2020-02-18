c2269d9c40b291d6b882693123a8a9c0f2c1128e
Akhilesh <akleshrailsdev@gmail.com>
2020-02-18 15:13:32 +0530

    Add comment in migration

--- db/migrate/20200214101258_add_participant_organizer.rb
+++ db/migrate/20200214101258_add_participant_organizer.rb
@@ -1,5 +1,6 @@
 class AddParticipantOrganizer < ActiveRecord::Migration[5.2]
   def change
+    # move existing data of participant's organizer_id into middle table (ParticipantOrganizer)
     participants = Participant.where.not(organizer_id: nil)
     participants.each do |participant|
       ParticipantOrganizer.create(participant_id: participant.id,
