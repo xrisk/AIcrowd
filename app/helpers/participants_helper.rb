1b64a3b38ec8cedb728389d675be1437c3f2b850
Akhilesh <akleshrailsdev@gmail.com>
2020-02-20 18:51:58 +0530

    Add color css and title

---
+++ app/helpers/participants_helper.rb
@@ -0,0 +1,23 @@
+module ParticipantsHelper
+  def avatar_url(participant, size)
+    if participant.avatar
+      if size == 'profile'
+        participant.avatar.url('thumbnail')
+      else
+        participant.avatar.url(size)
+      end
+    else
+      'https://www.gravatar.com/avatar/?d=mm&s=200'
+    end
+  end
+  # usage:  <%= image_tag avatar_url(user,'medium'), class: "img-responsive" %>
+  #         <%= image_tag avatar_url(user,'thumbnail') %>
+
+  def participant_link(participant)
+    participant.present? ? participant_path(participant) : '#'
+  end
+
+  def variation_class
+    ['variation-1', 'variation-2', 'variation-3'].sample
+  end
+end
