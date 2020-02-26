1d144d0e4464b891d6f2657e10a60ceeca51465d
Akhilesh <akleshrailsdev@gmail.com>
2020-02-26 15:39:54 +0530

    feature test cases for Invite participant

--- spec/features/challenges/private_challenge_spec.rb
+++ spec/features/challenges/private_challenge_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 describe "public user accesses challenge", :js do
-  # TODO
+  let!(:organizer)   { create :organizer }
+  let!(:participant) { create :participant, organizer: organizer }
+  let!(:challenge)   { create :challenge, organizer: organizer }
+
+  describe "Logged in user should invite participant" do
+    before do
+      log_in(participant)
+      visit "/challenges/#{challenge.id}/edit?step=private-challenge"
+    end
+
+    it "Invite participant by email" do
+      click_button '+ Invite participant'
+      fill_in 'challenge[invitation_email]', with: 'participant1@user.com, participant2@user.com'
+
+      click_button 'Update challenge'
+      expect(page).to have_text "Challenge updated."
+    end
+  end
 end
