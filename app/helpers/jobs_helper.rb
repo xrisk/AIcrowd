da1630387709a500d0f665da5601bed5daf499cb
piotrekpasciak <piotrek.pasciak@gmail.com>
2020-02-26 10:15:22 +0100

    Remove unused code with html_safe or raw methods

--- app/helpers/jobs_helper.rb
+++
@@ -1,6 +0,0 @@
-module JobsHelper
-  def flag(country)
-    alpha3 = ISO3166::Country.find_country_by_alpha2(country).alpha3.downcase
-    "<span class='flag flag-#{alpha3}'></span>".html_safe if alpha3.present?
-  end
-end
--- app/helpers/leaderboard_helper.rb
+++ app/helpers/leaderboard_helper.rb
@@ -1,26 +1,4 @@
 module LeaderboardHelper
-  def status_badge(status)
-    case status
-    when 'ready', 'submitted', 'initialized'
-      klass = 'badge-amber'
-    when 'graded'
-      klass = 'badge-green'
-    when 'failed'
-      klass = 'badge-red'
-    else
-      klass = 'badge-amber'
-    end
-
-    %(
-      <div class="#{klass}">
-        <span class="badge-square"></span>
-        <span class="badge-text">
-          #{status.capitalize}
-        </span>
-      </div>
-    ).html_safe
-  end
-
   def leaderboard_ranking_change(leaderboard)
     if leaderboard.previous_row_num.blank? ||
          leaderboard.previous_row_num == 0 ||
