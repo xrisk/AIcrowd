606bff157715121040a885b384e8033356bc0297
Akhilesh <akleshrailsdev@gmail.com>
2020-03-06 15:29:26 +0530

    requested changes

--- app/models/category_challenge.rb
+++ app/models/category_challenge.rb
@@ -2,5 +2,4 @@ class CategoryChallenge < ApplicationRecord
   belongs_to :category
   belongs_to :challenge
   validates :challenge_id, uniqueness: { scope: :category_id }
-  validates :challenge_id, :category_id, presence: true
 end
