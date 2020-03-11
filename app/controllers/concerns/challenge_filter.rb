2521688c672322b0c3bd85868ac429d80b5980c0
Akhilesh <akleshrailsdev@gmail.com>
2020-03-11 13:33:07 +0530

    Use dig on filter

--- app/controllers/concerns/challenge_filter.rb
+++ app/controllers/concerns/challenge_filter.rb
@@ -11,21 +11,23 @@ module ChallengeFilter
   end
 
   def filter_challenge(challenges = nil)
+    # category filter
     challenges = category? ? Challenge.joins(:categories).where('categories.id IN (?)', params[:category]['category_ids']) : all_list(challenges)
+    # status filter
     challenges = challenges.where(status_cd: params[:status]) if params[:status].present?
-    if params[:prize].present?
-      challenges = challenges.where.not(prize_cash: nil) if params[:prize]['prize_type'].include?('prize_cash')
-      challenges = challenges.where.not(prize_travel: nil) if params[:prize]['prize_type'].include?('prize_travel')
-      challenges = challenges.where.not(prize_academic: nil) if params[:prize]['prize_type'].include?('prize_academic')
-      challenges = challenges.where.not(prize_misc: nil) if params[:prize]['prize_type'].include?('prize_misc')
-    end
+    # prize filter
+    challenges = challenges.where.not(prize_cash: nil) if params.dig(:prize, 'prize_type')&.include?('prize_cash')
+    challenges = challenges.where.not(prize_travel: nil) if params.dig(:prize, 'prize_type')&.include?('prize_travel')
+    challenges = challenges.where.not(prize_academic: nil) if params.dig(:prize, 'prize_type')&.include?('prize_academic')
+    challenges = challenges.where.not(prize_misc: nil) if params.dig(:prize, 'prize_type')&.include?('prize_misc')
+    # return filtered challenges
     challenges
   end
 
   private
 
   def category?
-    params[:category] && params[:category]['category_ids'].present?
+    params.dig(:category, 'category_ids').present?
   end
 
   def all_list(challenges)
