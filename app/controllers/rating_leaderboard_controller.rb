11543d148bcc7b670e6b5f73b8cb095e0b5607b8
Nikhil Rayaprolu <nikhil684@gmail.com>
2020-03-14 13:33:04 +0530

    fixing bugs related to rating system

--- app/controllers/rating_leaderboard_controller.rb
+++ app/controllers/rating_leaderboard_controller.rb
@@ -2,7 +2,12 @@ class RatingLeaderboardController < ApplicationController
   def index
     rating_query = ParticipantRatingRanksQuery.new
     @count = rating_query.participants_count
-    @participants = rating_query.participants_with_ranks.page(params[:page]).per(10)
-    @user_position_participants = rating_query.user_position_participants(current_participant)
+    if params[:page].to_i < 10
+      @participants = rating_query.participants_with_ranks.page(params[:page]).per(10)
+      @user_position_participants = rating_query.user_position_participants(current_participant)
+    else
+      @participants = nil
+      @user_position_participants = nil
+    end
   end
 end
---
+++ db/migrate/20200314060809_add_ranking_change_to_participant.rb
@@ -0,0 +1,5 @@
+class AddRankingChangeToParticipant < ActiveRecord::Migration[5.2]
+  def change
+    add_column :participants, :ranking_change, :integer, :null => false, :default => 0
+  end
+end
