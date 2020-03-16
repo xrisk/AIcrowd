732333f8175560a8332f784cdb26bd75dff07a35
Akhilesh <akleshrailsdev@gmail.com>
2020-03-16 13:24:20 +0530

    Refactor test case

--- spec/services/challenges/filter_service_spec.rb
+++ spec/services/challenges/filter_service_spec.rb
@@ -1,22 +1,18 @@
 require 'rails_helper'
 
 describe Challenges::FilterService do
+  subject { described_class.new(params, Challenge.all) }
 
   describe '#call' do
-    let(:challenge1) { create(:challenge) }
-    let(:challenge2) { create(:challenge) }
+    let!(:challenge1) { create(:challenge) }
+    let!(:challenge2) { create(:challenge) }
     let(:category) { create(:category) }
-    let(:category_challenge) { create(:category_challenge, challenge: challenge1, category: category) }
-    let(:params) { create(:category_challenge, challenge: challenge1, category: category) }
+    let!(:category_challenge) { create(:category_challenge, challenge: challenge1, category: category) }
+    let(:params) { ActionController::Parameters.new(category: { category_names: [category.name] }, status: challenge1.status_cd, prize: { prize_type: challenge1.prize_cash }) }
 
     context 'when categories name, status and prize cash are provided' do
       it 'return challenges as per given fields' do
-        params = ActionController::Parameters.new(category: { category_names: [category.name] }, status: challenge1.status_cd, prize: { prize_type: challenge1.prize_cash })
-        challenge1
-        challenge2
-        category_challenge
-        subject = described_class.new(params, Challenge.all)
-        result  = subject.call
+        result = subject.call
         expect(result.first).to eq challenge1
         expect(result.count).to eq 1
       end
