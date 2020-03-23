3eed4707211b93a80b5514c7888d21e26fba9e51
piotrekpasciak <piotrek.pasciak@gmail.com>
2020-03-23 11:38:20 +0100

    Handle NewsletterForm submit in newsletter_emails#create action

--- app/controllers/challenges/newsletter_emails_controller.rb
+++ app/controllers/challenges/newsletter_emails_controller.rb
@@ -1,11 +1,45 @@
 module Challenges
   class NewsletterEmailsController < Challenges::BaseController
+    before_action :set_newsletter_email_groups, only: [:new, :create]
+
     def new
       @newsletter_email_form = NewsletterEmailForm.new
     end
 
     def create
+      @newsletter_email_form = NewsletterEmailForm.new(newsletter_email_form_params.merge(challenge: @challenge))
 
+      if @newsletter_email_form.save
+        redirect_to new_challenge_newsletter_emails_path(@challenge), flash: { notice: 'E-mail will be sent to participants after admin approval.' }
+      else
+        render :new
+      end
+    end
+
+    private
+
+    def set_newsletter_email_groups
+      @newsletter_email_groups = default_groups + challenge_rounds_groups
+    end
+
+    def default_groups
+      [
+        ['All participants', :all_participants],
+        ['Participants with submission', :participants_with_submission]
+      ]
+    end
+
+    def challenge_rounds_groups
+      @challenge_rounds.map do |challenge_round|
+        [
+          "Participants of \"#{challenge_round.challenge_round}\"",
+          challenge_round.id
+        ]
+      end
+    end
+
+    def newsletter_email_form_params
+      params.require(:newsletter_email_form).permit(:group, :cc, :bcc, :subject, :message)
     end
   end
 end
--- app/forms/newsletter_email_form.rb
+++ app/forms/newsletter_email_form.rb
@@ -1,24 +1,62 @@
 class NewsletterEmailForm
   include ActiveModel::Model
 
-  attr_accessor :to, :cc, :bcc, :subject, :message
+  attr_accessor :group, :cc, :bcc, :subject, :message, :challenge
 
-  validates :to, :subject, :message, presence: true
+  validates :group, :subject, :message, presence: true
 
   # def initialize(params= {})
   #   @newsletter_email = NewsletterEmail.new
   #   super(params)
   # end
 
-  # def call
-  #   return false if invalid?
-  #   # send acknowledgement reply, and admin notification emails, etc
-  #   true
-  # end
+  def save
+    return false if invalid?
 
-  # private
+    return false if cc_emails.empty? && bcc_emails.empty?
 
-  # def person_is_valid
-  #   errors.add(:base, 'is invalid') if newsletter_email.invalid?
-  # end
+    NewsletterEmail.create!(
+      emails_list: bcc_emails,
+      cc:          cc_emails,
+      subject:     subject,
+      message:     message
+    )
+
+    true
+  end
+
+  private
+
+  def cc_emails
+    @cc_emails ||= cc.split(',').map(&:strip)
+  end
+
+  def bcc_emails
+    @bcc_emails ||= bcc.split(',').map(&:strip) | map_group_to_emails
+  end
+
+  def map_group_to_emails
+    case group
+    when :all_participants # All Participants
+      challenge.participants.pluck(:email)
+    when :participants_with_submission # Participants who made at least one submission
+      Participant.joins(:submissions)
+        .where(submissions: { challenge_id: challenge.id })
+        .distinct
+        .map(&:email)
+    else # Participants who made at least one submission in selected round
+      Participant.joins(:submissions)
+        .where(submissions: { challenge_round_id: group })
+        .distinct
+        .map(&:email)
+    end
+  end
 end
+
+# All participants: All the users who have participated in this challenge/showed interest. (challenge_participant table)
+# Participants will submissions: All users who have made at least one submission
+# Participants of “Round 1”: All users who have at least one submission in Round 1
+# Participants of “Round X”: All users who have at least one submission in Round X
+# (populate based on the names of rounds in the challenge)
+# Participant.joins(:submissions).where(submissions: { challenge_id: challenge.id }).distinct.map(&:email)
+# Participant.joins(:submissions).where(submissions: { challenge_round_id: group }).distinct.map(&:email)
