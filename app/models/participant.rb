9c4515a95a828b1116c38d3286247b3f6a501ce7
Nikhil Rayaprolu <nikhil684@gmail.com>
2020-03-25 16:45:39 +0530

    implemneting tooltip for the challenge round

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -126,7 +126,7 @@ class Participant < ApplicationRecord
       account_disabled_dttm:   nil)
   end
   def user_rating_history
-    UserRating.joins(:challenge_round).where(participant_id: self.id).group_by_day(:end_dttm).maximum(:rating).reject { |_, v| v.nil? }
+    UserRating.joins("left outer join challenge_rounds on (challenge_rounds.id=challenge_round_id)").joins("left outer join challenges c on (c.id=challenge_rounds.challenge_id)").where(participant_id: 1).where('rating is not null').reorder('coalesce(end_dttm, user_ratings.created_at)').pluck('coalesce(end_dttm, user_ratings.created_at)', 'rating', 'concat(challenge, challenge_round)')
   end
 
   def active_for_authentication?
