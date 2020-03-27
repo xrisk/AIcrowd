1ab11be3663c2722dfeded742cc3623e3bf8c974
Akhilesh <akleshrailsdev@gmail.com>
2020-03-27 19:16:35 +0530

    Improve gif sharing

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -1,4 +1,6 @@
 module MetaTagsHelper
+  include MediaHelper
+  
   def meta_title
     if content_for?(:meta_title)
       content_for(:meta_title)
@@ -16,15 +18,27 @@ module MetaTagsHelper
   end
 
   def meta_image
-    meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META["meta_image"])
-    # little twist to make it work equally with an asset or a url
-    meta_image.starts_with?("http") ? meta_image : url_to_image(meta_image)
+    if show_action?
+      if controller_name == 'submissions'
+        having_media_image?(@challenge, @submission) ? s3_public_url(@submission, :large) : content_for_meta_image 
+      elsif controller_name == 'challenges' && @challenge.image_file?
+        @challenge.image_file.url
+      elsif controller_name == 'organizers' && @organizer.image_file?
+        @organizer.image_file.url
+      elsif controller_name == 'participants' && @participant.image_file?
+        @participant.image_file.url
+      end  
+    else  
+      content_for_meta_image
+    end
   end
 
   private
 
   def title_from_controller_name
-    case controller.controller_name
+    case controller_name
+    when 'organizers'
+      'Organizers'
     when 'challenges'
       'Challenges'
     when 'leaderboards'
@@ -44,5 +58,19 @@ module MetaTagsHelper
     when 'blogs'
       'Blog'
     end
+  end
+
+  def show_action?
+    controller.action_name == 'show'
+  end
+
+  def controller_name
+    controller.controller_name
+  end
+
+  def content_for_meta_image
+    meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META["meta_image"])
+    # little twist to make it work equally with an asset or a url
+    meta_image.starts_with?("http") ? meta_image : url_to_image(meta_image)
   end
 end
