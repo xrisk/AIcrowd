c70a7cb685c154b5cfda7c4254f750472bb4954e
piotrekpasciak <piotrek.pasciak@gmail.com>
2020-03-27 14:52:20 +0100

    Add placeholder to newsletter_email form

--- app/javascript/stimulus_js/controllers/select2_controller.js
+++ app/javascript/stimulus_js/controllers/select2_controller.js
@@ -3,8 +3,10 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   connect() {
     const requestUrl = this.data.get('url')
+    const placeholder = this.data.get('placeholder')
 
     $(this.element).select2({
+      placeholder: placeholder,
       ajax: {
         delay:    100,
         url:      requestUrl,
