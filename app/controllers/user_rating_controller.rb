11873437e897ea22903115042e29220217070911
Nikhil Rayaprolu <nikhil684@gmail.com>
2020-03-31 17:07:27 +0530

    resolving the codeclimate error

--- app/controllers/user_rating_controller.rb
+++ app/controllers/user_rating_controller.rb
@@ -6,7 +6,7 @@ class UserRatingController < ApplicationController
     ranks, teams_rating, teams_variation, teams_participant_ids = user_rating_service.filter_leaderboard_stats leaderboard_rating_stats
     teams_participant_ids.map do |participant_id|
       participant = Participant.find_by(id: participant_id[0])
-      user_final_rating =  UserRating.where("participant_id=#{participant_id[0]} and rating is not null and challenge_round_id is not null").joins(:challenge_round).reorder('user_ratings.created_at desc').select("user_ratings.*, challenge_rounds.end_dttm").first
+      user_final_rating =  UserRating.where(["participant_id=? and rating is not null and challenge_round_id is not null","#{participant_id[0]}" ]).joins(:challenge_round).reorder('user_ratings.created_at desc').select("user_ratings.*, challenge_rounds.end_dttm").first
       if user_final_rating.present?
         time_difference = (challenge_round.end_dttm.to_date - user_final_rating[:end_dttm].to_date)
         time_difference = time_difference.to_i
