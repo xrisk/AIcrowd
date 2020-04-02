af2be90202a4f7cea2d9f6b2cbee085418e086c1
Nikhil Rayaprolu <nikhil684@gmail.com>
2020-04-02 14:55:19 +0530

    adding few more badges

---
+++ app/jobs/badges_daily_job.rb
@@ -0,0 +1,60 @@
+class BadgesDailyJob < ApplicationJob
+  queue_as :default
+  def assign_badges_to_challenge(round_id)
+    user_rating_service = UserRatingService.new(round_id)
+    leaderboard_rating_stats = user_rating_service.leaderboard_query
+    ranks, teams_mu, teams_sigma, teams_participant_ids = user_rating_service.filter_leaderboard_stats leaderboard_rating_stats
+    max_rank = ranks.last
+    participant_ids, participant_ranks  = [], []
+    during_badge = 0
+    if user_rating_service.temporary
+      during_badge = 3
+    end
+
+    teams_participant_ids.each_with_index do |team, i|
+      team.each_with_index do |participant_id, j|
+        participant_ids << participant_id
+        participant_ranks << ranks[i].to_f/max_rank.to_f
+        if max_rank < 100
+          if participant_ranks < 0.1
+            Participant.find_by(id: participant_id).add_badge(3 + during_badge)
+          elsif participant_ranks < 0.2
+            Participant.find_by(id: participant_id).add_badge(2 + during_badge)
+          elsif participant_ranks < 0.4
+            Participant.find_by(id: participant_id).add_badge(1 + during_badge)
+          end
+        elsif max_rank > 100 && max_rank < 250
+          if participant_ranks < 0.1
+            Participant.find_by(id: participant_id).add_badge(3 + during_badge)
+          elsif participant_ranks < 0.2
+            Participant.find_by(id: participant_id).add_badge(2 + during_badge)
+          elsif participant_ranks < 0.4
+            Participant.find_by(id: participant_id).add_badge(1 + during_badge)
+          end
+        elsif max_rank > 250 && max_rank < 1000
+          if participant_ranks < 0.01
+            Participant.find_by(id: participant_id).add_badge(3 + during_badge)
+          elsif participant_ranks < 0.05
+            Participant.find_by(id: participant_id).add_badge(2 + during_badge)
+          elsif participant_ranks < 0.1
+            Participant.find_by(id: participant_id).add_badge(1 + during_badge)
+          end
+        elsif max_rank > 1000
+          if participant_ranks < 0.01
+            Participant.find_by(id: participant_id).add_badge(3 + during_badge)
+          elsif participant_ranks < 0.05
+            Participant.find_by(id: participant_id).add_badge(2 + during_badge)
+          elsif participant_ranks < 0.1
+            Participant.find_by(id: participant_id).add_badge(1 + during_badge)
+          end
+        end
+      end
+    end
+  end
+  def perform(*args)
+    challenge_rounds = ChallengeRound.where("calculated_permanent=FALSE OR calculated_permanent is NULL").order(end_dttm: :asc).pluck(:id)
+    challenge_rounds.each do |round_id|
+      assign_badges_to_challenge(round_id)
+    end
+  end
+end
--- config/initializers/merit.rb
+++ config/initializers/merit.rb
@@ -11,7 +11,7 @@ Merit.setup do |config|
 
   # Define :user_model_name. This model will be used to grand badge if no
   # `:to` option is given. Default is 'User'.
-  # config.user_model_name = 'User'
+  config.user_model_name = 'Participant'
 
   # Define :current_user_method. Similar to previous option. It will be used
   # to retrieve :user_model_name object if no `:to` option is given. Default
@@ -31,3 +31,150 @@ end
 # }].each do |attrs|
 #   Merit::Badge.create! attrs
 # end
+Merit::Badge.create!(
+    id: 1,
+    name: "gold-challenge-end",
+    description: "Gold Medal winner for a challenge",
+)
+Merit::Badge.create!(
+    id: 2,
+    name: "silver-challenge-end",
+    description: "Silver Medal winner for a challenge",
+    )
+Merit::Badge.create!(
+    id: 3,
+    name: "bronze-challenge-end",
+    description: "Bronze Medal winner for a challenge",
+    )
+Merit::Badge.create!(
+    id: 4,
+    name: "gold-challenge-during",
+    description: "Gold Medal during a challenge",
+    )
+Merit::Badge.create!(
+    id: 5,
+    name: "silver-challenge-during",
+    description: "Silver Medal during a challenge",
+    )
+Merit::Badge.create!(
+    id: 6,
+    name: "bronze-challenge-during",
+    description: "Bronze Medal during a challenge",
+    )
+Merit::Badge.create!(
+    id: 7,
+    name: "gold-submitter",
+    description: "Has made more than 1000 successful submissions",
+    )
+Merit::Badge.create!(
+    id: 8,
+    name: "silver-submitter",
+    description: "Has made more than 100 successful submissions",
+    )
+Merit::Badge.create!(
+    id: 9,
+    name: "bronze-submitter",
+    description: "Has made more than 10 submissions",
+    )
+Merit::Badge.create!(
+    id: 13,
+    name: "Beginner",
+    description: "Has made first submission",
+    )
+Merit::Badge.create!(
+    id: 14,
+    name: "Learner",
+    description: "Has made first successful submission",
+    )
+
+Merit::Badge.create!(
+    id: 15,
+    name: "Trustable",
+    description: "Has Filled his profile page",
+    )
+
+
+#Help Required from other team members
+Merit::Badge.create!(
+    id: 17,
+    name: "Team Player",
+    description: "For every 1 Team and first successful submission by the team",
+    )
+
+Merit::Badge.create!(
+    id: 18,
+    name: "Highest Streak",
+    description: "For Maximum number of consecutive days",
+    )
+
+Merit::Badge.create!(
+    id: 19,
+    name: "Current Streak",
+    description: "For current number of consecutive days",
+    )
+
+#After addition of user referral feature
+Merit::Badge.create!(
+    id: 16,
+    name: "Well Wisher",
+    description: "For every 1 user reffered",
+    )
+
+
+#After addition of a feature to track this
+Merit::Badge.create!(
+    id: 20,
+    name: "Benefactor",
+    description: "For every 1 challenge submission he has open-sourced",
+    )
+
+
+#Discourse
+Merit::Badge.create!(
+    id: 21,
+    name: "Curious",
+    description: "Has made first post on discourse",
+    )
+Merit::Badge.create!(
+    id: 22,
+    name: "Explainer",
+    description: "Has 10 posts/comments on Dicourse",
+    )
+
+Merit::Badge.create!(
+    id: 23,
+    name: "Explainer",
+    description: "For every 100 posts/comments on Dicourse",
+    )
+
+Merit::Badge.create!(
+    id: 24,
+    name: "Illumninated",
+    description: "For every 1000 posts/comments on Dicourse",
+    )
+
+Merit::Badge.create!(
+    id: 25,
+    name: "Refiner",
+    description: "For every 100 upvotes/downvotes on Dicourse",
+    )
+
+Merit::Badge.create!(
+    id: 26,
+    name: "Gold-Discussions",
+    description: "For every Discussion with 10 votes",
+    )
+
+Merit::Badge.create!(
+    id: 27,
+    name: "Silver-Discussions",
+    description: "For every Discussion with 5 votes",
+    )
+Merit::Badge.create!(
+    id: 28,
+    name: "Bronze-Discussions",
+    description: "For every Discussion with 1 vote",
+    )
+
+
+
