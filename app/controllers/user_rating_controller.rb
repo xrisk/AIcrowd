f5be118d1b7751fd8d8bb6444822f08a76c7ed1a
Nikhil Rayaprolu <nikhil684@gmail.com>
2020-04-08 12:34:42 +0530

    adding auth admin key function

--- app/controllers/user_rating_controller.rb
+++ app/controllers/user_rating_controller.rb
@@ -1,29 +1,24 @@
 class UserRatingController < ApplicationController
   before_action :auth_by_admin_api_key
+
+  def auth_by_admin_api_key
+    authenticate_or_request_with_http_token do |token, options|
+      (token == ENV['CROWDAI_API_KEY'])
+    end
+  end
+
   def get_leaderboard_ranks
     user_rating_service = UserRatingService.new(params[:round_id])
-    challenge_round = ChallengeRound.find_by(id:params[:round_id])
     leaderboard_rating_stats = user_rating_service.leaderboard_query
-    ranks, teams_rating, teams_variation, teams_participant_ids = user_rating_service.filter_leaderboard_stats leaderboard_rating_stats
-    teams_participant_ids.map do |participant_id|
-      participant = Participant.find_by(id: participant_id[0])
-      user_final_rating =  UserRating.where(["participant_id=? and rating is not null and challenge_round_id is not null","#{participant_id[0]}" ]).joins(:challenge_round).reorder('user_ratings.created_at desc').select("user_ratings.*, challenge_rounds.end_dttm").first
-      if user_final_rating.present?
-        time_difference = (challenge_round.end_dttm.to_date - user_final_rating[:end_dttm].to_date)
-        time_difference = time_difference.to_i
-        total_number_of_days = 365
-        updated_rating = participant.fixed_rating * (Math.exp(-time_difference/total_number_of_days))
-        participant.update!({rating: updated_rating})
-        UserRating.create!(participant_id: participant.id, rating: updated_rating, variation: user_final_rating['variation'], challenge_round: nil, created_at: challenge_round.end_dttm)
-        ranks, teams_rating, teams_variation, teams_participant_ids = user_rating_service.filter_leaderboard_stats leaderboard_rating_stats
-        end
-      end
+    puts leaderboard_rating_stats
+    ranks, teams_mu, teams_sigma, teams_participant_ids = user_rating_service.filter_leaderboard_stats leaderboard_rating_stats
     response = {
         ranks: ranks,
-        teams_rating: teams_rating,
-        teams_variation: teams_variation,
+        teams_mu: teams_mu,
+        teams_sigma: teams_sigma,
         teams_participant_ids: teams_participant_ids
     }
+    puts response
     render json: response.to_json
   end
   def post_new_participant_ratings
@@ -36,11 +31,9 @@ class UserRatingController < ApplicationController
     end
     user_rating_service = UserRatingService.new(params[:round_id])
     teams_participant_ids, new_team_ratings, new_team_variations = params[:participant_ids], params[:final_rating], params[:final_variation]
-    if new_team_ratings.present? && new_team_variations.present?
+    if new_team_ratings.blank? && new_team_variations.blank?
       participant_ids, new_participant_ratings, new_participant_variations = user_rating_service.filter_rating_api_output teams_participant_ids, new_team_ratings, new_team_variations
       user_rating_service.update_database_columns participant_ids, new_participant_ratings, new_participant_variations
-    elsif !teams_participant_ids.present?
-      user_rating_service.update_challenge_permanent
     end
   end
 end
