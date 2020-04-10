7732da094640a86280472cb2bd1e475c99c9765b
Nikhil Rayaprolu <nikhil684@gmail.com>
2020-04-10 22:33:21 +0530

    user rating attr_accessor fix

--- app/services/user_rating_service.rb
+++ app/services/user_rating_service.rb
@@ -1,6 +1,6 @@
 class UserRatingService
+  attr_accessor :temporary
   def initialize(round_id)
-    attr_accessor :temporary
     @round = ChallengeRound.find_by(id:round_id)
     if @round&.end_dttm.present? && @round.end_dttm < Time.current
       @temporary = false
