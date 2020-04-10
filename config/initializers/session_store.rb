f172bfc93967e4801f3db28adfb81f426268d84e
Akhilesh <akleshrailsdev@gmail.com>
2020-04-10 16:46:36 +0530

    Fix for test env

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-if Rails.env.development?
+if Rails.env.development? || Rails.env.test?
   Rails.application.config.session_store :cookie_store, key: '_aicrowd_session'
 else
   Rails.application.config.session_store :cookie_store, key: '_aicrowd_session', domain: ENV['DOMAIN_NAME'].gsub("https://www.", "").gsub("https://", "")
