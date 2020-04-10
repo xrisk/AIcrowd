f7bce2962577f07a28d883f87c6f93b36401ae4c
Akhilesh <akleshrailsdev@gmail.com>
2020-04-10 14:23:29 +0530

    set domain for config session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_aicrowd_session'
+if Rails.env.development?
+  Rails.application.config.session_store :cookie_store, key: '_aicrowd_session'
+else
+  Rails.application.config.session_store :cookie_store, key: '_aicrowd_session', domain: ENV['DOMAIN_NAME'].gsub("https://www.", "").gsub("https://", "")
+end
