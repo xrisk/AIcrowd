d486ffd59b469129d30a0fc38cbf67bc18cf196a
Akhilesh <akleshrailsdev@gmail.com>
2020-04-13 15:23:44 +0530

    Prefix Hash and change category to tags

--- app/helpers/challenges_helper.rb
+++ app/helpers/challenges_helper.rb
@@ -59,7 +59,7 @@ module ChallengesHelper
 
   def filter_message(status, category, prize)
     text  = "Filter challenges "
-    text += "[from #{category[:category_names].join(', ').humanize} category] " if category.present?
+    text += "[from ##{category[:category_names].join(', #').humanize} tag] " if category.present?
     text += "[with status #{status.humanize}] " if status.present?
     text += "[prizes in form of #{prize[:prize_type].join(', ').humanize}]" if prize.present?
     text
@@ -76,7 +76,16 @@ module ChallengesHelper
   end
 
   def categories_select_options(challenge)
-    options_from_collection_for_select(Category.all, :name, :name, { selected: challenge.categories.pluck(:name) })
+    challenge_category = challenge.categories.pluck(:name)
+    raw_options        = ''
+    Category.pluck(:name).each do |category_name|
+      if challenge_category.include?(category_name)
+        raw_options << "<option selected='selected' value='#{category_name}''>##{category_name}</option>"
+      else
+        raw_options << "<option value='#{category_name}''>##{category_name}</option>"
+      end
+    end
+    raw(raw_options)
   end
 
   private
