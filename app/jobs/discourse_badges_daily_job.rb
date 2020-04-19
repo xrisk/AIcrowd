256e4e7256c49f35be7853ea6a2e5bb314b35a2a
Nikhil Rayaprolu <nikhil684@gmail.com>
2020-04-19 15:14:46 +0530

    badges and rating fixes

--- app/jobs/discourse_badges_daily_job.rb
+++ app/jobs/discourse_badges_daily_job.rb
@@ -1,16 +1,20 @@
 class DiscourseBadgesDailyJob < ApplicationJob
   queue_as :default
+  def get_badges()
+    previous_id = DiscourseUserBadgesMetum.order(:created_at).last&.previous_id.to_i
+    Discourse::FetchBadgesService.new(previous_id).call.value
+  end
   def perform(*args)
     discourse_initial_id = ENV['DISCOURSE_INITIAL_ID'].to_i
-    previous_id = DiscourseUserBadgesMetum.order(:created_at).last&.previous_id.to_i
-    user_badges = Discourse::FetchBadgesService.new(previous_id).call.value
-    user_badges.each do |user_badge|
-      participant = Participant.find_by(name: user_badge['username'])
-      if participant
-        participant.add_badge(discourse_initial_id.to_i + user_badge['badge_id'].to_i)
+    while user_badges = get_badges && user_badges.present? do
+      user_badges.each do |user_badge|
+        participant = Participant.find_by(name: user_badge['username'])
+        if participant
+          participant.add_badge(discourse_initial_id.to_i + user_badge['badge_id'].to_i)
+        end
       end
+      last_id = user_badges.last['id']
+      DiscourseUserBadgesMetum.create(previous_id: last_id)
     end
-    last_id = user_badges.last['id']
-    DiscourseUserBadgesMetum.create(previous_id: last_id)
   end
 end
