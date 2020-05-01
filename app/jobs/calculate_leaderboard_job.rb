b6b3022b2d3735b75b28fa07758bbbb9548a6c09
Shivam Khandelwal <skbly7@gmail.com>
2020-05-01 09:19:40 +0530

    Added leaderboaard computation query for meta challenge

--- app/jobs/calculate_leaderboard_job.rb
+++ app/jobs/calculate_leaderboard_job.rb
@@ -4,8 +4,13 @@ class CalculateLeaderboardJob < ApplicationJob
   def perform(challenge_round_id:)
     if ChallengeRound.find(challenge_round_id).challenge.challenge == "NeurIPS 2019 : Disentanglement Challenge"
       NewCalculateLeaderboardService.new(challenge_round_id: challenge_round_id).call
+    elsif ChallengeRound.find(challenge_round_id).challenge.meta_challenge
+      CalculateMetaLeaderboardService.new(challenge_id: ChallengeRound.find(challenge_round_id).challenge.id).call
     else
       CalculateLeaderboardService.new(challenge_round_id: challenge_round_id).call
+      ChallengeProblems.where(challenge_round_id: challenge_round_id).each do |challenge_problem|
+        CalculateMetaLeaderboardService.new(challenge_id: challenge_problem.challenge_id).call
+      end
     end
   end
 end
