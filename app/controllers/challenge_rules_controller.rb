5a881354d2f2c54556a677e958968527e4990c50
piotrekpasciak <piotrek.pasciak@gmail.com>
2020-05-07 16:08:53 +0200

    Allow unlogged users to visit challenge rules page

--- app/controllers/challenge_rules_controller.rb
+++ app/controllers/challenge_rules_controller.rb
@@ -1,5 +1,4 @@
 class ChallengeRulesController < ApplicationController
-  before_action :authenticate_participant!
   before_action :set_challenge_rules
 
   def show; end
@@ -15,10 +14,12 @@ class ChallengeRulesController < ApplicationController
                          @challenge.current_challenge_rules
                        end
 
-    @challenge_participant =
+    if current_participant.present?
+      @challenge_participant =
       ChallengeParticipant
         .where(challenge_id: @challenge.id, participant_id: current_participant.id)
         .first_or_create
-    @challenge_participant.challenge_rules_accepted_version = @challenge_rules.version
+      @challenge_participant.challenge_rules_accepted_version = @challenge_rules.version
+    end
   end
 end
