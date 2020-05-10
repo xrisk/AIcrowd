f8a99f634a3a4e6a4ef8267663b36900ded60c54
piotrekpasciak <piotrek.pasciak@gmail.com>
2020-05-10 15:11:52 +0200

    Improve IntercomService

--- app/services/intercom_service.rb
+++ app/services/intercom_service.rb
@@ -1,40 +1,35 @@
 class IntercomService
-  def initialize
-    begin
-      @client = Intercom::Client.new(token: ENV['INTERCOM_TOKEN'])
-    rescue Intercom::MisconfiguredClientError => e
-      Rails.logger.info "Intercom: #{e.message}"
-      raise
-    end
+  def initialize(event_name, participant, metadata = {})
+    @event_name  = event_name
+    @participant = participant
+    @metadata    = metadata
+    @client      = Intercom::Client.new(token: ENV['INTERCOM_TOKEN'])
+  rescue Intercom::MisconfiguredClientError => e
+    Rails.logger.info "Intercom: #{e.message}"
+    raise
+  end
 
-    def call(event_name, participant, metadata = {})
-      if participant&.email.present? and metadata.is_a?(Hash)
-        begin
-          @client.events.create(
-              event_name: event_name,
-              created_at: Time.now.to_i,
-              email: participant.email,
-              metadata: metadata
-          )
-        rescue Intercom::ResourceNotFound => e
-          @client.contacts.create(email: participant.email, role: "user")
-          @client.events.create(
-              event_name: event_name,
-              created_at: Time.now.to_i,
-              email: participant.email,
-              metadata: metadata
-          )
-        rescue Intercom::IntercomError => e
-          Rails.logger.info "Intercom: #{e.message}"
-          raise
-        end
-      elsif !participant&.email.present?
-        Rails.logger.info "Intercom: Email for the participant or missing"
-      elsif !participant.present?
-        Rails.logger.info "Intercom: Participant nil"
-      elsif !metadata.is_a?(Hash)
-        Rails.logger.info "Intercom: Metadata is not a string"
-      end
-    end
+  def call
+    return Rails.logger.info "Intercom: Participant nil" if @participant.blank?
+    return Rails.logger.info "Intercom: Metadata is not a hash" unless @metadata.is_a?(Hash)
+
+    create_event_request
+  rescue Intercom::ResourceNotFound => e
+    @client.contacts.create(email: @participant.email, role: "user")
+    create_event_request
+  rescue Intercom::IntercomError => e
+    Rails.logger.info "Intercom: #{e.message}"
+    raise e
+  end
+
+  private
+
+  def create_event_request
+    @client.events.create(
+      event_name: @event_name,
+      created_at: Time.current.to_i,
+      email:      @participant.email,
+      metadata:   @metadata
+    )
   end
 end
--- spec/services/intercom_service_spec.rb
+++ spec/services/intercom_service_spec.rb
@@ -1,45 +1,44 @@
 require 'rails_helper'
 
-RSpec.describe IntercomService do
+describe IntercomService, focus: true do
   let!(:participant) { create :participant }
-
-  describe '#initialize' do
-    ENV['INTERCOM_TOKEN'] = 'TEST_TOKEN'
-    subject { described_class.new }
-
-    it { expect(subject).to be_a described_class }
-    it { expect(subject.instance_variable_get(:@client)).to be_a Intercom::Client }
-  end
 
   describe '#call' do
     before(:context) do
       Intercom::Client.contacts.create(email: participant.email, role: "user")
     end
+
     context 'with a null token' do
       ENV["INTERCOM_TOKEN"] = nil
       subject { described_class.new }
       it { expect(subject).to raise_exception Intercom::MisconfiguredClientError }
     end
+
     context 'with no event name' do
       subject { described_class.new.call('', participant, {}) }
       it { expect(subject).to raise_exception Intercom::IntercomError }
     end
+
     context 'with a right event name' do
       subject { described_class.new.call('test_event', participant, {}) }
       it { expect(subject).to be_a nil }
     end
+
     context 'with a participant not a contact of intercom' do
       subject { described_class.new.call('test_event', participant, {}) }
       it { expect(subject).to be_a nil }
     end
+
     context 'with no participant' do
       subject { described_class.new.call('test_event', nil, {}) }
       it { expect(subject).to be_a nil }
     end
+
     context 'with a participant not having email' do
       subject { described_class.new.call('test_event', participant, {}) }
       it { expect(subject).to be_a nil }
     end
+
     context 'with metadata has a hash' do
       subject { described_class.new.call('test_event', participant, 'sht') }
       it { expect(subject).to be_a nil }
