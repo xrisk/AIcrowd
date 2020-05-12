bd5012f92c3a42aa5439571f3bca89d4b2eb5ad6
Nikhil Rayaprolu <nikhil684@gmail.com>
2020-05-12 19:59:41 +0530

    changing the typos

--- spec/services/intercom_service_spec.rb
+++ spec/services/intercom_service_spec.rb
@@ -5,7 +5,7 @@ describe IntercomService do
 
   describe '#call' do
     context 'with no event name'  do
-      it 'returns raises excetion of Intercom' do
+      it 'returns raises exception of Intercom' do
         VCR.use_cassette('intercom_service/with_no_event_name') do
           expect { IntercomService.new('', participant).call }.to raise_exception Intercom::UnexpectedError
         end
@@ -29,7 +29,7 @@ describe IntercomService do
     end
 
     context 'with no participant' do
-      it 'returns raises excetion of Intercom' do
+      it 'returns null with right event name' do
         VCR.use_cassette('intercom_service/with_no_participant') do
           expect(IntercomService.new('test_event', nil, {}).call).to eq nil
         end
@@ -37,7 +37,7 @@ describe IntercomService do
     end
 
     context 'with a participant not having email' do
-      it 'returns raises excetion of Intercom' do
+      it 'returns null with right event name' do
         VCR.use_cassette('intercom_service/with_participant_not_having_email') do
           expect(IntercomService.new('test_event', participant, {}).call).to eq nil
         end
@@ -45,7 +45,7 @@ describe IntercomService do
     end
 
     context 'with metadata has a hash' do
-      it 'returns raises excetion of Intercom' do
+      it 'returns null with right event name' do
         VCR.use_cassette('intercom_service/with_non_hash_metadata') do
           expect(IntercomService.new('test_event', participant, 'sht').call).to eq nil
         end
