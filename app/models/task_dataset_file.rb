95430d68b891b027499a9bf1f17c80437de4f254
piotrekpasciak <piotrek.pasciak@gmail.com>
2020-05-15 19:56:39 +0200

    Fix undefined_method excecption TaskDatasetFile objects

--- app/models/task_dataset_file.rb
+++ app/models/task_dataset_file.rb
@@ -2,5 +2,7 @@ class TaskDatasetFile < ApplicationRecord
   belongs_to :clef_task
   has_many :task_dataset_file_downloads, dependent: :destroy
 
+  attr_accessor :error_message
+
   validates :title, presence: true
 end
