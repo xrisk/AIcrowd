3a58df6d6af9670b1f65e44939780334ed2496e2
Nikhil Rayaprolu <nikhil684@gmail.com>
2020-06-04 23:20:33 +0530

    changes requested

--- db/migrate/20200604112701_add_default_to_badge_type_in_badges.rb
+++
@@ -1,5 +0,0 @@
-class AddDefaultToBadgeTypeInBadges < ActiveRecord::Migration[5.2]
-  def change
-    change_column :aicrowd_badges, :badge_type_id, :integer, :default => 4, :null => false
-  end
-end
