8f57999bd9baa78e67d4361118d8515163c7d8d4
piotrekpasciak <piotrek.pasciak@gmail.com>
2020-06-15 09:45:36 +0200

    Add UUID suffix to eua files

--- app/uploaders/eua_uploader.rb
+++ app/uploaders/eua_uploader.rb
@@ -5,6 +5,12 @@ class EuaUploader < CarrierWave::Uploader::Base
     "EUAs/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def filename
+    full_name = original_filename.to_s
+
+    "#{File.basename(full_name, File.extname(full_name))}_#{SecureRandom.uuid}.#{file&.extension}"
+  end
+
   def extension_whitelist
     ['pdf']
   end
