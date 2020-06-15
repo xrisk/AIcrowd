a99b5cd5c2efc88d166138931ae419173eac4889
piotrekpasciak <piotrek.pasciak@gmail.com>
2020-06-15 14:11:39 +0200

    Fix params in CalculateLeaderboardJob

--- app/jobs/calculate_leaderboard_job.rb
+++ app/jobs/calculate_leaderboard_job.rb
@@ -7,10 +7,10 @@ class CalculateLeaderboardJob < ApplicationJob
     elsif ChallengeRound.find(challenge_round_id).challenge.meta_challenge
       CalculateMetaLeaderboardService.new(challenge_id: ChallengeRound.find(challenge_round_id).challenge.id).call
     else
-      ChallengeRounds::CreateLeaderboardsService.new(challenge_round_id: challenge_round_id).call
+      ChallengeRounds::CreateLeaderboardsService.new(challenge_round: ChallengeRound.find(challenge_round_id)).call
       # Trigger all the leaderboard computations in case this round_id is being used for meta challenge
       ChallengeProblems.where(challenge_round_id: challenge_round_id).each do |challenge_problem|
-        ChallengeRounds::CreateLeaderboardsService.new(challenge_round_id: challenge_round_id, meta_challenge_id: challenge_problem.challenge_id).call
+        ChallengeRounds::CreateLeaderboardsService.new(challenge_round: ChallengeRound.find(challenge_round_id), meta_challenge_id: challenge_problem.challenge_id).call
         CalculateMetaLeaderboardService.new(challenge_id: challenge_problem.challenge_id).call
       end
     end
