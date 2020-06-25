c938b1bb459c95bc616826e6005f1cc6d0de6565
Karthik Viswanathan <karthik.viswanathan@research.iiit.ac.in>
2020-06-26 00:15:43 +0530

    Sidekiqized badge jobs

--- app/jobs/participant_badge_job.rb
+++ app/jobs/participant_badge_job.rb
@@ -2,7 +2,17 @@ class ParticipantBadgeJob < ApplicationJob
   queue_as :default
 
   def perform(name: , participant_id: nil, grading_status_cd: nil, submission_id: nil)
-    @participant = Participant.find_by(id: participant_id)
+    @submission = nil
+    
+    if submission_id.present?
+        @submission = Submission.find(submission_id)
+    end
+    
+    if participant_id.present?
+        @participant = Participant.find(participant_id)
+    elsif @submission.present?
+        @participant = @submission.participant
+      
     for badge in AicrowdBadge.where(badges_event_id: BadgesEvent.where(name: name).pluck(:id))
       eval(badge.code)
     end
