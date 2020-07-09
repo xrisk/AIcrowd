297037b7163507effe54b35b4be0c8d66f923de9
piotrekpasciak <piotrek.pasciak@gmail.com>
2020-07-09 14:16:33 +0200

    Test Review apps failing

--- db/migrate/20200701124812_create_participant_ml_challenge_goals.rb
+++ db/migrate/20200701124812_create_participant_ml_challenge_goals.rb
@@ -1,9 +1,12 @@
 class CreateParticipantMlChallengeGoals < ActiveRecord::Migration[5.2]
   def change
     create_table :participant_ml_challenge_goals do |t|
-      t.references :participant, foreign_key: true
-      t.references :challenge, foreign_key: true
-      t.references :daily_practice_goal, foreign_key: true
+      t.references :participant
+      # , foreign_key: true
+      t.references :challenge
+      # , foreign_key: true
+      t.references :daily_practice_goal
+      # , foreign_key: true
 
       t.timestamps
     end
