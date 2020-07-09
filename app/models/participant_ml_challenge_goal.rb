1a13af60f5f215504c305c963996965029f9a8ee
Akhilesh <akleshrailsdev@gmail.com>
2020-07-09 20:48:13 +0530

    quick fix

--- app/models/participant_ml_challenge_goal.rb
+++ app/models/participant_ml_challenge_goal.rb
@@ -1,5 +1,5 @@
 class ParticipantMlChallengeGoal < ApplicationRecord
-  belongs_to :participant
+  belongs_to :participant, optional: true
   belongs_to :challenge
   belongs_to :daily_practice_goal
 end
