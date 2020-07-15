9b928b2495c7d1ed2f534178de41add69e163c46
piotrekpasciak <piotrek.pasciak@gmail.com>
2020-07-15 15:22:40 +0200

    Allow to create participant_organizer by providing participant name in params

--- app/serializers/api/v1/participant_serializer.rb
+++ app/serializers/api/v1/participant_serializer.rb
@@ -7,22 +7,8 @@ module Api
 
       def serialize
         {
-          id:                              participant.id,
-          email:                           participant.email,
-          name:                            participant.name,
-          confirmed_at:                    participant.confirmed_at,
-          admin:                           participant.admin,
-          verified:                        participant.verified,
-          bio:                             participant.bio,
-          website:                         participant.website,
-          github:                          participant.github,
-          linkedin:                        participant.linkedin,
-          twitter:                         participant.twitter,
-          affiliation:                     participant.affiliation,
-          country_cd:                      participant.country_cd,
-          rating:                          participant.rating,
-          agreed_to_marketing:             participant.agreed_to_marketing,
-          agreed_to_organizers_newsletter: participant.agreed_to_organizers_newsletter
+          id:   participant.id,
+          name: participant.name
         }
       end
 
