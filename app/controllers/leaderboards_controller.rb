b15539df3bf64b4f70c7ef36175d387e800392da
shell-x <73057914+shell-x@users.noreply.github.com>
2020-10-23 13:05:43 +0530

    pin user rank on challenges leaderboard

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -19,6 +19,15 @@ class LeaderboardsController < ApplicationController
     end
     @top_three_winners = @leaderboards.where(baseline: false).first(3)
 
+    if current_participant.present?
+      team_id = get_team_id(@challenge, @current_participant.id)
+      if team_id.present?
+        @self_standing    = @leaderboards.where(submitter_id: team_id, submitter_type: 'Team')
+      else
+        @self_standing    = @leaderboards.where(submitter_id: @current_participant.id, submitter_type: 'Participant')
+      end
+    end
+
     if params[:country_name].present? || params[:affiliation].present?
       @leaderboards = @leaderboards.where(id: @filter.call('leaderboard_ids'))
       @leaderboards = paginate_leaderboards_by(:seq)
@@ -147,7 +156,18 @@ class LeaderboardsController < ApplicationController
   end
 
   def paginate_leaderboards_by(order)
-    @leaderboards.page(params[:page]).per(20).order(order)
+    nextpage = @leaderboards.page(params[:page]).per(20).order(order)
+
+    if @self_standing.present? && nextpage.present?
+        nextpage.each do |row|
+          if @self_standing.ids.include? row.id
+            @self_standing = nil
+            break
+          end
+        end
+    end
+
+    nextpage
   end
 
   def set_filter_service
@@ -163,4 +183,16 @@ class LeaderboardsController < ApplicationController
 
     (policy(@challenge).edit? || current_participant&.admin)
   end
+
+  def get_team_id(challenge, participant_id)
+    team = challenge.teams.joins(:team_participants).find_by(team_participants: { participant_id: participant_id})
+
+    if team.present?
+      team.id
+    else
+      nil
+    end
+  end
+
+
 end
