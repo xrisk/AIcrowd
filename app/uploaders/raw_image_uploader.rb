d13c468275e71db68dac36a3f811cd929f7b9629
Sujnesh Mishra <sujneshmishra@gmail.com>
2020-11-10 15:05:02 +0530

    create unique name for images

--- app/uploaders/raw_image_uploader.rb
+++ app/uploaders/raw_image_uploader.rb
@@ -10,4 +10,12 @@ class RawImageUploader < CarrierWave::Uploader::Base
   def extension_white_list
     ['jpg', 'jpeg', 'gif', 'png', 'svg']
   end
+
+  def filename
+    random_token = Digest::SHA2.hexdigest("#{Time.now.utc}--#{model.id.to_s}").first(20)
+    ivar = "@#{mounted_as}_secure_token"
+    token = model.instance_variable_get(ivar)
+    token ||= model.instance_variable_set(ivar, random_token)
+    "#{model.id}_#{token}.jpg" if original_filename
+  end
 end
