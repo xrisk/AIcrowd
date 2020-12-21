890a983decc2588dd877b740ca8bdf7239af3534
Shivam Khandelwal <skbly7@gmail.com>
2020-12-21 21:59:07 +0530

    Bugfix: Submission ordering in case of dynamic score fields

--- app/services/challenge_rounds/create_leaderboards_service.rb
+++ app/services/challenge_rounds/create_leaderboards_service.rb
@@ -257,12 +257,12 @@ module ChallengeRounds
 
       score_field = "score"
       if @challenge_leaderboard_extra.dynamic_score_field.present?
-        score_field = "meta->>'#{@challenge_leaderboard_extra.dynamic_score_field}'"
+        score_field = "CAST(meta->>'#{@challenge_leaderboard_extra.dynamic_score_field}' as decimal)"
       end
 
       score_secondary_field = "score_secondary"
       if @challenge_leaderboard_extra.dynamic_score_secondary_field.present?
-        score_secondary_field = "meta->>'#{@challenge_leaderboard_extra.dynamic_score_secondary_field}'"
+        score_secondary_field = "CAST(meta->>'#{@challenge_leaderboard_extra.dynamic_score_secondary_field}' as decimal)"
       end
 
       score_sort_order = sort_map(@challenge_leaderboard_extra.primary_sort_order_cd)
