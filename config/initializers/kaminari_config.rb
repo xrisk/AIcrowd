8053c3f11b12020f681533fb172b62084c0295cc
Sujnesh Mishra <sujneshmishra@gmail.com>
2020-12-24 14:32:38 +0530

    active admin kaminari and will_paginate issue

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -13,3 +13,17 @@ Kaminari.configure do |config|
   # config.params_on_first_page = false
   config.page_method_name = :per_page_kaminari
 end
+
+if defined?(WillPaginate)
+  module WillPaginate
+    module ActiveRecord
+      module RelationMethods
+        def per(value = nil) per_page(value) end
+        def total_count() count end
+      end
+    end
+    module CollectionMethods
+      alias_method :num_pages, :total_pages
+    end
+  end
+end
