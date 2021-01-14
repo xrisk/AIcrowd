8f029ed9e25ac2a4653ecc8380ff5de744578f9e
Sujnesh Mishra <sujneshmishra@gmail.com>
2021-01-15 00:21:34 +0530

    add tags to community contributions

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,8 @@
 class Category < ApplicationRecord
   has_many :category_challenges, dependent: :destroy
   has_many :challenges, through: :category_challenges
+  has_many :category_posts, dependent: :destroy
+  has_many :posts, through: :category_posts
 
   validates :name, presence: true, format: { without: /#/, message: " => you can't add # in Tags" }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,13 +3,15 @@ class Post < ApplicationRecord
   include FriendlyId
   friendly_id :title, use: :slugged
   has_many :likes, as: :reference, dependent: :destroy
+  has_many :category_posts, dependent: :destroy
+  has_many :categories, through: :category_posts
 
   belongs_to :participant
   belongs_to :challenge, optional: true
   belongs_to :submission, optional: true
   mount_uploader :thumbnail, RawImageUploader
   acts_as_commontable
-  attr_accessor :notebook_file_path, :notebook_file
+  attr_accessor :notebook_file_path, :notebook_file, :category_names
 
 
 
