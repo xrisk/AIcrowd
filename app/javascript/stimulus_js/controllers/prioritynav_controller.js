c452892dc2a44c837c86389d039c03c80c0e0713
Shivam Khandelwal <skbly7@gmail.com>
2021-01-28 01:34:31 +0530

    Navbar rendering fixes and added conditions for mobile devices

--- app/javascript/stimulus_js/controllers/prioritynav_controller.js
+++ app/javascript/stimulus_js/controllers/prioritynav_controller.js
@@ -6,16 +6,15 @@ export default class extends Controller {
     }
 }
 
-// https://css-tricks.com/diy-priority-plus-nav/
 let PriorityNav = {
     init: function() {
         this.menu = $(".priority-nav");
-        this.allNavElements = $(".priority-nav > ul > li:not('.overflow-nav')");
         this.bindUIActions();
         this.setupMenu();
     },
 
     setupMenu: function() {
+        PriorityNav.allNavElements = $(".priority-nav > ul > li:not('.overflow-nav')");
         let firstPos = $(".priority-nav > ul > li:first").position();
         let wrappedElements = $();
         let first = true;
@@ -32,12 +31,43 @@ let PriorityNav = {
         });
 
         if (wrappedElements.length) {
-            let newSet = wrappedElements.clone();
+            let newSet = wrappedElements.clone().find('a');
             wrappedElements.addClass("d-none");
             newSet.addClass("dropdown-item");
+            newSet.removeClass("nav-link");
             $(".overflow-nav-list").append(newSet);
             $(".overflow-nav").addClass("show-inline-block");
             $(".priority-nav").css("overflow", "visible");
+
+            if($(window).width() < 500) {
+                let navButtons = $('.sub-nav-bar a.btn:not(:last)');
+                if(navButtons.length) {
+                    let extraSet = navButtons.clone();
+                    extraSet.removeClass();
+                    extraSet.addClass("dropdown-item");
+                    $('.overflow-nav-list').append('<hr class="m-0" />');
+                    $('.overflow-nav-list').append(extraSet);
+                    navButtons.addClass('d-none btn-none');
+                    navButtons.removeClass('btn');
+                }
+
+                let navButtonsInDropdown = $('.priority-nav a.dropdown-item').not('.priority-nav ul.nav a.dropdown-item');
+                if(navButtonsInDropdown.length) {
+                    let extraSet = navButtonsInDropdown.clone();
+                    extraSet.removeClass();
+                    extraSet.addClass("dropdown-item");
+                    $('.overflow-nav-list').append('<hr class="m-0" />');
+                    $('.overflow-nav-list').append(extraSet);
+                    navButtonsInDropdown.addClass('d-none dropdown-a-none');
+                    navButtonsInDropdown.removeClass('btn');
+                    navButtonsInDropdown.parent('.dropdown-menu').parent('.dropdown').addClass('d-none dropdown-a-none');
+                }
+            }
+
+            let menuButton = $(".priority-nav ul.nav .dropdown-toggle");
+            if($(".priority-nav > ul > li:not(.d-none)").length == 1 && menuButton) {
+                menuButton.html(menuButton.html().replace('More','Menu'));
+            }
         }
         else {
             $(".overflow-nav").addClass("d-none");
@@ -47,6 +77,14 @@ let PriorityNav = {
     tearDown: function() {
         $(".overflow-nav-list").empty();
         $(".overflow-nav").removeClass("d-none show-inline-block");
+        $(".btn-none").addClass("btn").removeClass("btn-none d-none");
+        $(".dropdown-a-none").removeClass("dropdown-a-none d-none");
+
+        let menuButton = $(".priority-nav ul.nav .dropdown-toggle");
+        if(menuButton) {
+            menuButton.html(menuButton.html().replace('Menu','More'));
+        }
+
         PriorityNav.allNavElements.removeClass("d-none");
     },
 
@@ -63,4 +101,10 @@ let PriorityNav = {
                 PriorityNav.menu.removeClass("overflow-hidden");
             }, 500));
     }
+};
+
+window.nav_bar_reset = function() {
+    PriorityNav.tearDown();
+    PriorityNav.setupMenu();
+    PriorityNav.menu.removeClass("overflow-hidden");
 };
