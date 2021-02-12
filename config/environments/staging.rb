58f87a6c8821b07dc32695af3af12d4912555a77
Sujnesh Mishra <sujneshmishra@gmail.com>
2021-02-12 16:37:57 +0530

    remove global expiration/memcahe take care of it

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
 
   config.active_record.dump_schema_after_migration = false
 
-  config.cache_store = :mem_cache_store, { expires_in: 6.hours},
+  config.cache_store = :mem_cache_store,
                        (ENV['MEMCACHEDCLOUD_SERVERS'].to_s).split(','),
                        {
                          username: ENV['MEMCACHEDCLOUD_USERNAME'],
