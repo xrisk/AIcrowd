05e5863085df1f99b27b80fac4205d9280e2c957
Sujnesh Mishra <sujneshmishra@gmail.com>
2021-03-16 19:53:22 +0530

    bifurcate the submission into uniquely named folders

--- app/services/notebooks/submission_notebook_service.rb
+++ app/services/notebooks/submission_notebook_service.rb
@@ -4,6 +4,7 @@ module Notebooks
     include S3FilesHelper
 
     def initialize(submission)
+      @folder_name = "#{SecureRandom.uuid}"
       submission_file = submission.submission_files.first
       @url = submission_file.submission_file_s3_key
       @file_name = submission_file.submission_file_s3_key.split('/').last.split('.').first
@@ -16,15 +17,14 @@ module Notebooks
 
       filename = File.basename(notebook_file_path)
       html_filename = filename.chomp(File.extname(filename)) + (".html")
-      html_filename_path = Rails.root.join('public', 'uploads', html_filename)
+      html_filename_path = Rails.root.join('public', 'uploads', @folder_name, html_filename)
 
       notebook_gist_url = `gist #{notebook_file_path}`
       notebook_s3_url = upload_to_s3(notebook_file_path, filename)
       notebook_html = File.read(html_filename_path).html_safe
       gist_id = notebook_gist_url.strip.gsub(ENV['GIST_URL'], "")
 
-      File.delete(notebook_file_path) if File.exist?(notebook_file_path)
-      File.delete(html_filename_path) if File.exist?(html_filename_path)
+      FileUtils.rm_rf(Rails.root.join('public', 'uploads', @folder_name))
       File.delete(Rails.root.join('public', 'uploads', "#{@file_name}.zip")) if File.exist?(Rails.root.join('public', 'uploads', "#{@file_name}.zip"))
 
       return {notebook_s3_url: notebook_s3_url, notebook_html: notebook_html, gist_id: gist_id}
@@ -50,12 +50,12 @@ module Notebooks
     def unzip_file(file_path)
       Zip::File.open(file_path) do |zip_file|
         zip_file.glob("#{@notebook_name}").each do |f|
-           f_path=File.join('public', 'uploads', f.name)
+           f_path=File.join('public', 'uploads', @folder_name, f.name)
            FileUtils.mkdir_p(File.dirname(f_path))
            zip_file.extract(f, f_path) unless File.exist?(f_path)
         end
       end
-      return Rails.root.join('public', 'uploads', @notebook_name)
+      return Rails.root.join('public', 'uploads', @folder_name, @notebook_name)
     end
 
   end
