bf0358147b21414e9a4bfd0edcb2739830cb9c46
Sujnesh Mishra <sujneshmishra@gmail.com>
2021-04-09 03:23:21 +0530

    leaderboard trigger on team participant commit

--- app/models/team_participant.rb
+++ app/models/team_participant.rb
@@ -7,4 +7,13 @@ class TeamParticipant < ApplicationRecord
   as_enum :role, ROLES, map: :string, source: :role, prefix: true
 
   validates :role, inclusion: { in: ROLES }
+
+  after_commit :recalculate_leaderboard, on: [:create, :update]
+
+  def recalculate_leaderboard
+    self.team.challenge.challenge_rounds.each do |challenge_round|
+      CalculateLeaderboardJob.perform_now(challenge_round_id: challenge_round.id)
+    end
+  end
+
 end
