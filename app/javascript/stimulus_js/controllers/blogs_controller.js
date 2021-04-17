4c2029708ba8feaca3978eef583965196c6574c0
Sujnesh Mishra <sujneshmishra@gmail.com>
2021-04-18 01:25:48 +0530

    js class change

--- app/javascript/stimulus_js/controllers/blogs_controller.js
+++ app/javascript/stimulus_js/controllers/blogs_controller.js
@@ -3,7 +3,7 @@ export default class extends Controller {
   scrollToComments(event){
     $('.button-scroll').click(function() {
         $('html,body').animate({
-            scrollTop: $(".blog-comments").offset().top},
+            scrollTop: $(".blog-discussion").offset().top},
             'slow');
     });
   }
