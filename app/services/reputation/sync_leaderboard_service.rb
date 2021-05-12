8999ce36afcc01458d4073ef1127345185c21263
Sujnesh Mishra <sujneshmishra@gmail.com>
2021-05-12 14:14:50 +0530

    added leaderboard type and correct ranking

--- app/services/reputation/sync_leaderboard_service.rb
+++ app/services/reputation/sync_leaderboard_service.rb
@@ -17,11 +17,11 @@ module Reputation
 
     def sync_data(last_updated_time)
       # base_leaderboards = BaseLeaderboard.where("created_at > ?", last_updated_time))
-      base_leaderboards = BaseLeaderboard.where(challenge_round_id: ChallengeRound.all.pluck(:id))
+      base_leaderboards = BaseLeaderboard.where(challenge_round_id: ChallengeRound.all.pluck(:id), leaderboard_type_cd: "leaderboard").where("submitter_id is not NULL").where("row_num is not NULL")
       base_leaderboards = rename_keys(base_leaderboards.as_json)
       base_leaderboards = base_leaderboards.map do |bl|
         if bl["challenge_round_id"] && bl["rank"] && bl["created_at"] && bl["participant_id"]
-          {"challenge_round_id": bl["challenge_round_id"], "rank": bl["rank"], "created_at": bl["created_at"], participant_id: bl["participant_id"]}
+          {"challenge_round_id": bl["challenge_round_id"], "rank": (bl["rank"] - 1), "created_at": bl["created_at"], participant_id: bl["participant_id"]}
         end
       end
       base_leaderboards.delete_if { |h| h.blank? }
