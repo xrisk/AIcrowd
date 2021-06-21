c1e3126295a459b37f37f47a15fd4ebe81ed8be9
Sujnesh Mishra <sujneshmishra@gmail.com>
2021-06-21 22:54:41 +0530

    initial setup for submission graph refresh

--- app/javascript/stimulus_js/controllers/submission_mermaid_controller.js
+++ app/javascript/stimulus_js/controllers/submission_mermaid_controller.js
@@ -7,6 +7,12 @@ export default class extends Controller {
       if(mermaid) {
         mermaid.init(".aicrowd-mermaid");
       }
+      setInterval(function () {
+        $('#submission-mermaid-div').load(window.location.href + " #submission-mermaid-div > *" );
+        if(mermaid) {
+          mermaid.init(".aicrowd-mermaid");
+        }
+      }, 30000);
     });
   }
 }
