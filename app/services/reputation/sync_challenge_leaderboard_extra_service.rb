ad7583070753a098b15b6fe06546c8896a5e19d0
Sujnesh Mishra <sujneshmishra@gmail.com>
2021-07-16 03:59:47 +0530

    id wise rank updation

--- app/services/reputation/sync_challenge_leaderboard_extra_service.rb
+++ app/services/reputation/sync_challenge_leaderboard_extra_service.rb
@@ -1,7 +1,8 @@
 module Reputation
   class SyncChallengeLeaderbaordExtraService
 
-    def initialize
+    def initialize cle_id
+      @cle_id = cle_id
     end
 
     def call
@@ -22,8 +23,7 @@ module Reputation
     end
 
     def sync_data
-      challenge_leaderboard_extras_ids = [93,94,95,96,174,194,195,198,191,192,170,171,172,204,205,206,207,200,201,202,203,208,209,210,211,115,116,188,189,98,99,110,104,106,212,50,51,52,520,190,185,220,321,294,296,298,164,301,149,145,53,59,60,168,157,158,159,160,161,162,154,155,156,140,348,324,337,325,322,361,366,300,279,55,503,383,77,407,408,409,410,413,45,46,518,43,44,523,435,439,443,468,769,461,56,478,479,480,511,512,513,514,517,553,554,600,556,558,591,618,619,620,621,622,799,798,753,760,631,648,649,651,653,774,675,797,795,796,773,687,689,690,691,696,697,705,709,744,793,794,787,788,772,775,784,785,790,801,802,803,804]
-      challenge_leaderboard_extras = ChallengeLeaderboardExtra.where(id: challenge_leaderboard_extras_ids).includes(:challenge_round)
+      challenge_leaderboard_extras = ChallengeLeaderboardExtra.where(id: @cle_id).includes(:challenge_round)
       result = []
       challenge_leaderboard_extras.each do |cle|
         if cle.challenge_round
