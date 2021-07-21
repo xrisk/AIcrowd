5b5f724fcacef0d4b3ba4c47515861a31260c603
Sujnesh Mishra <sujneshmishra@gmail.com>
2021-07-22 00:00:18 +0530

    gitlab-component integration

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,6 @@ require("chart.js");
 import '../react_components/index.js';
 import '../stimulus_js/index.js';
 // Support component names relative to this directory:
-var componentRequireContext = require.context("components", true);
+var componentRequireContext = require.context("../../../node_modules/aicrowd-components-library/src/components", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
