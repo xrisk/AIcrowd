f5f6068214f3a6774df95d616b7c2d0e00f521d8
Shivam Khandelwal <shivam@aicrowd.com>
2021-07-30 19:53:27 +0530

    Check for non-login users, remove :update, corrected variable

--- app/controllers/participants/sessions_controller.rb
+++ app/controllers/participants/sessions_controller.rb
@@ -1,7 +1,9 @@
 class Participants::SessionsController < Devise::SessionsController
-  after_action :save_user_profile_to_mixpanel, only: [:create, :update]
+  after_action :save_user_profile_to_mixpanel, only: [:create]
 
   def save_user_profile_to_mixpanel
-    Mixpanel::SyncJob.perform_later(current_user, request.remote_ip)
+    if resource.present?
+      Mixpanel::SyncJob.perform_later(resource, request.remote_ip)
+    end
   end
 end
