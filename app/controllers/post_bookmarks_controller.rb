20e205b4be1e1860c55bab6b277af53ce5f7685f
Shivam Khandelwal <skbly7@gmail.com>
2021-08-17 09:53:14 +0530

    Comments & Bookmark improvements, TOC styling

--- app/controllers/post_bookmarks_controller.rb
+++ app/controllers/post_bookmarks_controller.rb
@@ -5,6 +5,7 @@ class PostBookmarksController < ApplicationController
 
   def create
     PostBookmark.create!(post_id: params[:post_id], participant_id: current_participant.id)
+    @toast = true
 
     respond_to do |format|
       format.js { render :refresh }
@@ -12,10 +13,15 @@ class PostBookmarksController < ApplicationController
   end
 
   def destroy
-    PostBookmark.where(id: params[:id]).first.destroy
+    bookmark = PostBookmark.where(id: params[:id]).first
+    @toast = false
+    if bookmark.present? && bookmark.participant_id == current_participant.id
+        bookmark.destroy
+        @toast = true
+    end
 
     respond_to do |format|
-      format.js { render :refresh }
+      format.js { render :refresh, toast: true, locals: {toast: true}  }
     end
   end
 
