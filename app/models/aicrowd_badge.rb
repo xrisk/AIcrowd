3050be3b262cf43fd6eeefe9c33819baac23bf76
Sujnesh Mishra <sujneshmishra@gmail.com>
2021-08-26 19:54:48 +0530

    added popup changes and added csv for challenge badges

--- app/models/aicrowd_badge.rb
+++ app/models/aicrowd_badge.rb
@@ -3,7 +3,7 @@ class AicrowdBadge < ApplicationRecord
   has_many :aicrowd_user_badges
   belongs_to :badge_type
   belongs_to :badges_event
-  mount_uploader :image, LogoImageUploader
+  # mount_uploader :image, LogoImageUploader
   # validates :name, uniqueness: true, presence: true
   def badges_event_name
     BadgesEvent.find_by(id:self.badges_event_id)&.name
