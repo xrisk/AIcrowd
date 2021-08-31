5d0bd45433c0e1690b749f201316560ac3f4d3e8
Sujnesh Mishra <sujneshmishra@gmail.com>
2021-08-31 23:19:40 +0530

    deleted src folder for testing

--- app/javascript/src/constants/badge.js
+++
@@ -1,4 +0,0 @@
-export const achievementBadges = ['gold', 'silver', 'bronze'];
-export const namedBadges = ['admin', 'failed', 'graded', 'organizer', 'practice', 'problem-statement', 'submitted'];
-
-export const allBadgeTypes = [...achievementBadges, ...namedBadges, 'success', 'time-left'];
