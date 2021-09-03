05ef3d819957c90d13ebbffa957002bbfdbc0650
Sujnesh Mishra <sujneshmishra@gmail.com>
2021-09-03 14:36:48 +0530

    updated webpack configuration

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,14 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
-
-const myCssLoaderOptions = {
-  modules: true,
-  sourceMap: true,
-};
-
-const CSSLoader = environment.loaders.get('sass').use.find(el => el.loader === 'css-loader');
-
-CSSLoader.options = merge(CSSLoader.options, myCssLoaderOptions);
-
-module.exports = environment;
