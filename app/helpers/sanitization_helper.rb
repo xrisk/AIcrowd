4788406725538e287c51f2dc2964c3ae2d5f1bfb
Sujnesh Mishra <sujneshmishra@gmail.com>
2021-09-07 19:05:07 +0530

    corrected parser options

--- app/helpers/sanitization_helper.rb
+++ app/helpers/sanitization_helper.rb
@@ -1,6 +1,6 @@
 module SanitizationHelper
   def sanitize_html(html_content)
-    Sanitize.fragment(html_content, Sanitize::Config.merge(Sanitize::Config::RELAXED, :parser_options => {max_tree_depth: -1})).html_safe
+    Sanitize.fragment(html_content, Sanitize::Config.merge(Sanitize::Config::RELAXED, :parser_options => {max_errors: -1, max_tree_depth: -1})).html_safe
   end
 
   def sanitize_html_with_attr(html_content, elements: [], attributes: {})
