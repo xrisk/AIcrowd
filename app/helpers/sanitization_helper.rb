d5d2afc197cd724102622bd753ea53ce9afe9e6b
Sujnesh Mishra <sujneshmishra@gmail.com>
2021-09-14 11:13:44 +0530

    added document tree bug fix and notification for discourse badges

--- app/helpers/sanitization_helper.rb
+++ app/helpers/sanitization_helper.rb
@@ -4,6 +4,6 @@ module SanitizationHelper
   end
 
   def sanitize_html_with_attr(html_content, elements: [], attributes: {})
-    Sanitize.fragment(html_content, Sanitize::Config.merge(Sanitize::Config::RELAXED, elements: Sanitize::Config::BASIC[:elements] + elements, attributes: attributes)).html_safe
+    Sanitize.fragment(html_content, Sanitize::Config.merge(Sanitize::Config::RELAXED, elements: Sanitize::Config::BASIC[:elements] + elements, attributes: attributes, :parser_options => {max_errors: -1, max_tree_depth: -1})).html_safe
   end
 end
