0010a74262913c17fa529856c671cbdecb83b5ac
Sujnesh Mishra <sujneshmishra@gmail.com>
2021-12-14 13:48:01 +0530

    added condition for edit popup

--- app/helpers/participants_helper.rb
+++ app/helpers/participants_helper.rb
@@ -93,6 +93,6 @@ module ParticipantsHelper
   # Probability 1%
   # Have at least one challenge participant
   def show_participant_edit_popup?(participant)
-    participant.present? && participant.gender_cd.blank? && participant.affiliation.blank? && participant.challenge_participants.where(registered: true).exists?
+    participant.present? && rand(100) < 2 participant.gender_cd.blank? && participant.affiliation.blank? && participant.challenge_participants.where(registered: true).exists?
   end
 end
